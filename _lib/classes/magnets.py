2f4857982d0d080507f9531dec8322efe66c9210
lucasgcb <lucascoliado@hotmail.com>
2019-02-13 09:21:31 +0100

    Add base for standardization attempt

--- _lib/classes/magnets.py
+++ _lib/classes/magnets.py
@@ -27,6 +27,7 @@ class Cube(HomoMag):
     
     Class Initialization (only kwargs):
     ---------------------
+    dim : vec3 [mm]
     
     mag : vec3 [mT]
         Set magnetization vector of magnet in units of [mT].
