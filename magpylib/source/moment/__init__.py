3a01ada276f8d392681ce1e1dbc2d7a77b12a295
lucasgcb <lucascoliado@hotmail.com>
2019-02-27 17:52:50 +0100

    Fix numpy docstrings for moment package (issue #20)

--- magpylib/source/moment/__init__.py
+++ magpylib/source/moment/__init__.py
@@ -1,1 +1,10 @@
+"""
+Moment Sources
+==============
+   
+Moment sources for generating magnetic fields. 
+Compatible with :class:`~magpylib.Collection`
+"""
+__all__ = ["Dipole"] #This is for Sphinx
+
 from magpylib._lib.classes.moments import Dipole
