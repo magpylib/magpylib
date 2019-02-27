986a01077f84c5296a764cf045ee13cc613baa98
lucasgcb <lucascoliado@hotmail.com>
2019-02-27 17:56:12 +0100

    Add autosummary list to source class init docstring

--- magpylib/source/__init__.py
+++ magpylib/source/__init__.py
@@ -2,10 +2,39 @@
 Source Classes
 ==============
    
-Available sources for generating magnetic fields, 
+Available source types for generating magnetic fields, 
 compatible with :class:`~magpylib.Collection`
+
+.. currentmodule:: magpylib.source.magnet
+
+
+Quicklist for :mod:`~magpylib.source.magnet` sources:
+
+.. autosummary::
+
+   Box
+   Sphere
+   Cylinder
+
+.. currentmodule:: magpylib.source.current
+
+Quicklist for :mod:`~magpylib.source.current` sources:
+
+.. autosummary::
+
+   Line
+   Circular
+
+.. currentmodule:: magpylib.source.moment
+
+Quicklist for :mod:`~magpylib.source.moment` sources:
+
+.. autosummary::
+
+   Dipole
+
 """
-__all__ = ["magnet","current"] # This is for Sphinx
+__all__ = ["magnet","current","moment"] # This is for Sphinx
 
 
 import magpylib.source.magnet as magnet
