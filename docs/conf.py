e5b1033d84b8a9f81b44b0e3199268344a0839f0
lucasgcb <lucascoliado@hotmail.com>
2019-02-27 10:43:25 +0100

    Add Readme to exclude list, remove module pages

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ def setup(app):
                                         # hiding ">>>" in examples
     sphinx.apidoc.main(['-f', #Overwrite existing files
                         '-T', #Create table of contents
-                        '-e', #Give modules their own pages
+                        #'-e', #Give modules their own pages
                         '-E', #user docstring headers
                         '-M', #Modules first
                         '-o', #Output the files to:
@@ -94,7 +94,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_autogen/magpylib._lib*']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_autogen/magpylib._lib*','README*']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
