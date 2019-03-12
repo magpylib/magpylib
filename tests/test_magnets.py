81b17b3a1cd34a21589d9410ae349a059aa8b6db
Lucas Bandeira <lucascoliado@hotmail.com>
2019-03-12 15:40:16 +0100

    Add Multicore getB to Box ( close #5 ) (#57)
    - Add multicoreGetB(*pos) method
           calculates several positions concurrently
         - Add test for GetB
         - Add test for Multicore getB

--- tests/test_magnets.py
+++ tests/test_magnets.py
@@ -16,3 +16,38 @@ def test_BoxEdgeCase1():
     pm = magnet.Box(mag=[0,0,1000],dim=[0.5,0.1,1],pos=[.25,.55,-1111])
     result = pm.getB([.5,.5,5])
     assert any(isnan(result)), "Results from getB is not NaN"
+
+def test_BoxGetB():
+    erMsg = "Results from getB are unexpected"
+    mockResults = ( 3.99074612, 4.67238469, 4.22419432) ## Expected 3 results for this input
+    rounding = 4 ## Round for floating point error 
+    
+    from numpy import isnan, array
+    pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+    result = pm.getB([.5,.5,5])
+
+    for i in range(3):
+        assert round(result[i],rounding)==round(mockResults[i],rounding), erMsg
+
+def test_BoxMulticoreGetB():
+    erMsg = "Results from getB are unexpected"
+    pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+
+    ## Positions list
+    result = pm.multiGetB(  (.5,.5,5),
+                            (30,20,10),
+                            (1,.2,60), ) 
+
+    ## Expected Results
+    mockRes = ( ( 3.99074612, 4.67238469, 4.22419432), # .5,.5,.5
+                ( 0.03900578,  0.01880832, -0.00134112), # 30,20,10
+                ( -0.00260347, -0.00313962,  0.00610886), ) 
+
+    ## Rounding for floating point error 
+    rounding = 4 
+
+    # Loop through predicted cases and check if the positions from results are valid
+    for i in range(len(mockRes)):
+        for j in range(3):
+            assert round(result[i][j],rounding)==round(mockRes[i][j],rounding), erMsg
+    
