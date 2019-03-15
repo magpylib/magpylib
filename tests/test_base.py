e60b8f970233b6094fbb5484009cbb953ea0453f
Lucas Bandeira <lucascoliado@hotmail.com>
2019-03-15 09:33:42 +0100

    Update getB() to include multicore option (#63)
    * Update multicore getB structure for Base ( #5 )
        - actual getB is now a private method
          _getBPrime(*pos) which is a pure function
        - new getB() is an interface and calls the pure
          function for the user to run multi or
          sequential routines on _getBPrime()
         - multicore kwarg enables multicore
           processing. Default False
         - If multicore is enabled, workers kwarg
           overrides the automatic set value for
           concurrent processes
    
    * Add HIDE UNDOCUMENTED classes to Sphinx
    
    * Move List/Sequential tests to test_base.py
    
    * Update getBprime for multicore to magnets
        - Also add tests for multicore Sphere, Cylinder
    
    * Add getBprime for multicore to currents class
        - Also added tests for Line, Circular
    
    * Add getBprime for multicore to moments
        - Also added test for Dipole
    
    * fix typo on Sphere multicore test
    
    * Fix bug - arguments entered without being in a list
        - Accepts getB([2,3,6],[1,3,5],[3,5,6]) now
    
    * Remove *pos feature, update sequential list test

---
+++ tests/test_base.py
@@ -0,0 +1,102 @@
+from magpylib.source import magnet
+from numpy import isnan, array
+import pytest 
+
+def test_RCSGetB():
+    erMsg = "Results from getB are unexpected"
+    mockResults = ( 3.99074612, 4.67238469, 4.22419432) ## Expected 3 results for this input
+
+    # Input
+    mag=[6,7,8]
+    dim=[10,10,10]
+    pos=[2,2,2]
+
+    # Run
+    pm = magnet.Box(mag,dim,pos)
+    result = pm.getB([.5,.5,5])
+
+    rounding = 4 ## Round for floating point error 
+    for i in range(3):
+        assert round(result[i],rounding)==round(mockResults[i],rounding), erMsg
+
+def test_RCSGetBSequential():
+    erMsg = "Results from getB are unexpected"
+    mockResults = ( (3.99074612, 4.67238469, 4.22419432),
+                    (3.99074612, 4.67238469, 4.22419432),
+                    (3.99074612, 4.67238469, 4.22419432),)## Expected 3 results for this input
+
+    # Input
+    mag=[6,7,8]
+    dim=[10,10,10]
+    pos=[2,2,2]
+    fieldPos = [[.5,.5,5],
+                [.5,.5,5],
+                [.5,.5,5]]
+    # Run
+    pm = magnet.Box(mag,dim,pos)
+    result = pm.getB(fieldPos,multicore=False)
+
+    rounding = 4 ## Round for floating point error 
+    for i in range(len(mockResults)):
+        for j in range(3):
+            assert round(result[i][j],rounding)==round(mockResults[i][j],rounding), erMsg
+
+
+def test_RCSMulticoreGetB():
+    erMsg = "Results from getB are unexpected"
+    pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+
+    ## Positions list
+    result = pm.getB(   (.5,.5,5),
+                        (30,20,10),
+                        (1,.2,60),
+                        multicore=True ) 
+
+    ## Expected Results
+    mockRes = ( ( 3.99074612, 4.67238469, 4.22419432), # .5,.5,.5
+                ( 0.03900578,  0.01880832, -0.00134112), # 30,20,10
+                ( -0.00260347, -0.00313962,  0.00610886), ) 
+
+    ## Rounding for floating point error 
+    rounding = 4 
+
+    # Loop through predicted cases and check if the positions from results are valid
+    for i in range(len(mockRes)):
+        for j in range(3):
+            assert round(result[i][j],rounding)==round(mockRes[i][j],rounding), erMsg
+
+
+def test_RCSMulticoreGetBList():
+    erMsg = "Results from getB are unexpected"
+    pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+
+    ## Positions list
+    posList = [ (.5,.5,5),
+                [30,20,10],
+                [1,.2,60],]
+                
+    result = pm.getB(posList,multicore=True ) 
+
+    ## Expected Results
+    mockRes = ( ( 3.99074612, 4.67238469, 4.22419432), # .5,.5,.5
+                ( 0.03900578,  0.01880832, -0.00134112), # 30,20,10
+                ( -0.00260347, -0.00313962,  0.00610886), ) 
+
+    ## Rounding for floating point error 
+    rounding = 4 
+
+    # Loop through predicted cases and check if the positions from results are valid
+    for i in range(len(mockRes)):
+        for j in range(3):
+            assert round(result[i][j],rounding)==round(mockRes[i][j],rounding), erMsg
+
+def test_RCSGetBSequentialList():
+    erMsg = "Results from getB are unexpected"
+    pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+
+    ## Positions list
+    with pytest.raises(ValueError):         
+        result = pm.getB(   (.5,.5,5),
+                            [30,20,10],
+                            [1,.2,60],multicore=False ) 
+        
