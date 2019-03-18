dd000e51e72929ec08538597c9462df1eb293bdf
lucasgcb <lucascoliado@hotmail.com>
2019-03-18 10:34:47 +0100

    Change *pos to pos in getB()

--- tests/test_base.py
+++ tests/test_base.py
@@ -44,26 +44,27 @@ def test_RCSGetBSequential():
 
 def test_RCSMulticoreGetB():
     erMsg = "Results from getB are unexpected"
-    pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+    with pytest.raises(TypeError):
+        pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
+        
+        ## Positions list
+        result = pm.getB(   (.5,.5,5), #pylint: disable=redundant-keyword-arg
+                            (30,20,10),
+                            (1,.2,60),
+                            multicore=True ) 
 
-    ## Positions list
-    result = pm.getB(   (.5,.5,5),
-                        (30,20,10),
-                        (1,.2,60),
-                        multicore=True ) 
+        ## Expected Results
+        mockRes = ( ( 3.99074612, 4.67238469, 4.22419432), # .5,.5,.5
+                    ( 0.03900578,  0.01880832, -0.00134112), # 30,20,10
+                    ( -0.00260347, -0.00313962,  0.00610886), ) 
 
-    ## Expected Results
-    mockRes = ( ( 3.99074612, 4.67238469, 4.22419432), # .5,.5,.5
-                ( 0.03900578,  0.01880832, -0.00134112), # 30,20,10
-                ( -0.00260347, -0.00313962,  0.00610886), ) 
+        ## Rounding for floating point error 
+        rounding = 4 
 
-    ## Rounding for floating point error 
-    rounding = 4 
-
-    # Loop through predicted cases and check if the positions from results are valid
-    for i in range(len(mockRes)):
-        for j in range(3):
-            assert round(result[i][j],rounding)==round(mockRes[i][j],rounding), erMsg
+        # Loop through predicted cases and check if the positions from results are valid
+        for i in range(len(mockRes)):
+            for j in range(3):
+                assert round(result[i][j],rounding)==round(mockRes[i][j],rounding), erMsg
 
 
 def test_RCSMulticoreGetBList():
@@ -95,8 +96,8 @@ def test_RCSGetBSequentialList():
     pm = magnet.Box(mag=[6,7,8],dim=[10,10,10],pos=[2,2,2])
 
     ## Positions list
-    with pytest.raises(ValueError):         
-        pm.getB(   (.5,.5,5),
+    with pytest.raises(TypeError):         
+        pm.getB(   (.5,.5,5), #pylint: disable=redundant-keyword-arg
                     [30,20,10],
                     [1,.2,60],multicore=False ) 
         
