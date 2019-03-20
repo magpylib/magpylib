89ffb5e29347c1177ecd0b99408220c246e805a8
lucasgcb <lucascoliado@hotmail.com>
2019-03-20 09:52:26 +0100

    Fix collection issues with new multicore

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -2,7 +2,7 @@ import unittest
 from magpylib._lib.classes.collection import Collection
 from magpylib._lib.classes.base import RCS
 from numpy import array
-
+import pytest
 def test_collectionGetB():
     from magpylib._lib.classes.magnets import Box
 
@@ -35,19 +35,21 @@ def test_collectionGetBList():
 
     mag=(2,3,5)
     dim=(2,2,2)
-    pos=[   [2,2,2],
-            [2,2,2]]
+    pos=array([   [2,2,2],
+                [2,2,2]])
 
+
+    with pytest.raises(IndexError):
     #Run   
-    b = Box(mag,dim)
-    b2 = Box(mag,dim)
-    c = Collection(b,b2)
-    result = c.getB(pos) 
+        b = Box(mag,dim)
+        b2 = Box(mag,dim)
+        c = Collection(b,b2)
+        result = c.getB(pos) 
 
-    rounding = 4
-    for i in range(len(mockResult)):
-        for j in range(3):
-            assert round(result[i][j],rounding) == round(mockResult[i][j],rounding)
+        rounding = 4
+        for i in range(len(mockResult)):
+            for j in range(3):
+                assert round(result[i][j],rounding) == round(mockResult[i][j],rounding)
 
 def test_collectionGetBMulticoreList():
     from magpylib._lib.classes.magnets import Box
@@ -57,9 +59,9 @@ def test_collectionGetBMulticoreList():
 
     mag=(2,3,5)
     dim=(2,2,2)
-    pos=[   [2,2,2],
-            [2,2,2],
-            [2,2,3]]
+    pos=array([     [2,2,2],
+                    [2,2,2],
+                    [2,2,3]])
 
 
     mockResult = [  [0.24976596, 0.21854521, 0.15610372],
@@ -69,7 +71,7 @@ def test_collectionGetBMulticoreList():
     b = Box(mag,dim)
     b2 = Box(mag,dim)
     c = Collection(b,b2)
-    result = c.getB(pos,multicore=True) 
+    result = c.getBMulticore(pos)
 
     rounding = 4
     for i in range(len(mockResult)):
