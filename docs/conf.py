671b1c8fae9e9186969052e057c1bfba138d863d
lucasgcb <lucascoliado@hotmail.com>
2019-03-26 12:12:24 +0100

    Add first logo iteration to repository

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_logo = './_static/magpylib_logo.png'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
