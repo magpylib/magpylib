d0123dc5f26529421728a676eccf04eac5e97e93
Lucas Bandeira <lucascoliado@hotmail.com>
2019-05-17 15:29:18 +0200

    License setup (#119)
    * Add license
    
    * Add AGPL boilerplate to all source code
    
    * Update boilerplate in all files with disclaimer

--- magpylib/_lib/fields/PM_Box.py
+++ magpylib/_lib/fields/PM_Box.py
@@ -1,3 +1,26 @@
+# -------------------------------------------------------------------------------
+# MagPyLib -- A Python 3.2+ toolbox for working with magnetic fields.
+# Copyright (C) Silicon Austria Labs, https://silicon-austria-labs.com/,  
+#               Michael Ortner <magpylib@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program.  If not, see <https://www.gnu.org/licenses/>. 
+# The acceptance of the conditions of the GNU Affero General Public License are 
+# compulsory for the usage of the software.
+#
+# For contact information, reach out over at <magpylib@gmail.com> or our issues
+# page at https://www.github.com/magpylib/magpylib/issues.
+# -------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
 
 # MAGNETIC FIELD CALCULATION OF CUBE IN CANONICAL BASIS
