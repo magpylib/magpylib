cb0e91c6f0104dd1e921abe0ed614563b0c62dcc
Lucas Bandeira <lucascoliado@hotmail.com>
2019-06-04 14:02:51 +0200

    Update development with master (#149)
    * Add documentation link to installation
    
    * Add Windows and Python Environment CI, code coverage reporter (#137)
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Remove Python 2.7 from tests in Azure
    
    * Add upload to codecov in circleCI
    
    * separate dependencies by comma
    
    * separate deps by line break
    
    * Add codecoverage token
    
    * remove code coverage from azure
    
    * force token into tox for now
    
    * remove codecov-python
    
    * Fix environment variable
    
    * Update readme
    
    * add appveyor conf
    
    * move appveyor.yml
    
    * add pytest to install script
    
    * change appveyor to python3
    
    * add 3.5 python to appveyor
    
    * force python3 in appveyor
    
    * pants on head workaround for python3
    
    * Remove duplicate key from appveyor
    
    * add invocation for pytest
    
    * Add python versions to test, build later
    
    * define branches
    
    * Remove python 3.4 and older from tests for now
    
    * Update readme with badges and icon
    
    * Center badges and mascot
    
    * Update README.md
    
    * Add feature trigger to azure (#138)
    
    * Minimum Python Version fix (#139)
    
    * Add feature trigger to azure
    
    * Add Python 3.7 to appveyor
    
    * Update min Python version to 3.5
    
    * Lock dependency versions (#141)
    
    * Remove Python 3.5 from tests
    
    * Lock dependency versions
    
    * Adjust version disclaimer for Python3
    
    * Fix minimum Python version to 3.6
    
    * Update issue templates (#142)
    
    * Update issue templates
    
    * Create PULL_REQUEST_TEMPLATE.md
    
    * Remove minor Python version from description
    
    * Add permanent link to images
    
    * Add configuration for PyPi (#143)
    
    * Add PyPi Release classifiers
    
    * Remove odd center html tag
    
    * Update readme
    
    * Add PyPi badge
    
    * Add pypilink to badge
    
    * Put downloads at bottom
    
    * Add LICENSE to PyPi package (#144)

--- setup.py
+++ setup.py
@@ -17,15 +17,16 @@
 #   $ (packCondaTest) pip install .
 # The library is now in the packCondaTest environment.
 ##
-_SphinxVersion = "1.8.2"
+_magPyVersion = "1.0.2-beta"
 
+_SphinxVersion = "1.8.2"
 _name = "magpylib"
-_magPyVersion = "1.0.0-beta"
-_description = "A simple, user friendly Python 3.2+ toolbox for calculating magnetic fields from permanent magnets and current distributions."
+_description = "A simple, user friendly Python 3 toolbox for calculating magnetic fields from permanent magnets and current distributions."
 _author_email = "magpylib@gmail.com"
 _author = "Michael Ortner"
 _projectUrl = "https://github.com/magpylib/magpylib"
 _release = "beta"
+_license='GNU Affero General Public License v3 or later (AGPLv3+) (AGPL-3.0-or-later)'
 
 import setuptools
 
@@ -41,16 +42,26 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=_projectUrl,
+    license=_license,
     packages=setuptools.find_packages(),
     zip_safe = False, ## Gives the environment files so we can access docs, enables tooltips but may decrease performance
     install_requires=[
-          'numpy',
-          'matplotlib',
+          'numpy>=1.16',
+          'matplotlib>=3.1',
       ],
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Programming Language :: Python :: 3",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         "Operating System :: OS Independent",
     ],
+    python_requires='~=3.6',
+    keywords='magnetism physics analytical parallel electromagnetic fields b-field',
     command_options={
         'build_sphinx': {
             'project': ('setup.py', _name),
