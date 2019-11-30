e6ab3911e23313a75140ff74367bf369caea148e
Ortner Michael <Michael.Ortner@silicon-austria.com>
2019-11-30 23:08:44 +0100

    doku test css, fix example1

--- docs/pyplots/examples/01_SimpleCollection.py
+++ docs/pyplots/examples/01_SimpleCollection.py
@@ -1,33 +1,33 @@
-#imports
+# imports
 import numpy as np
 import matplotlib.pyplot as plt
 import magpylib as magpy
 
-#create figure
+# create figure
 fig = plt.figure(figsize=(8,4))
 ax1 = fig.add_subplot(121, projection='3d')
 ax2 = fig.add_subplot(122)
 
-#create magnets
+# create magnets
 s1 = magpy.source.magnet.Box(mag=[0,0,600],dim=[3,3,3],pos=[-4,0,3])
 s2 = magpy.source.magnet.Cylinder(mag=[0,0,500], dim=[3,5])
 
-#manipulate magnets
+# manipulate magnets
 s1.rotate(45,[0,1,0],anchor=[0,0,0])
 s2.move([5,0,-4])
 
-#create collection
+# create collection
 c = magpy.Collection(s1,s2)
 
-#display system geometry
+# display system geometry
 magpy.displaySystem(c,subplotAx=ax1,suppress=True)
 
-#calculate B-field on a grid
+# calculate B-field on a grid
 xs = np.linspace(-10,10,29)
 zs = np.linspace(-10,10,29)
 Bs = np.array([[c.getB([x,0,z]) for x in xs] for z in zs])
 
-#display field in xz-plane using matplotlib
+# display field in xz-plane using matplotlib
 X,Z = np.meshgrid(xs,zs)
 U,V = Bs[:,:,0], Bs[:,:,2]
 ax2.streamplot(X, Z, U, V, color=np.log(U**2+V**2),density=2)
