d1d26532d1604fe061d298be2e7bed6c119c3cab
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2019-12-01 03:07:32 -0300

    Add actual path for imports

--- magpylib/source/__init__.py
+++ magpylib/source/__init__.py
@@ -29,6 +29,6 @@ subpackages: `magnet`, `current` and `moment`.
 __all__ = ["magnet", "current", "moment"]  # This is for Sphinx
 
 #make these subpackages visible in ipython tooltips
-import magpylib.source.magnet as magnet
-import magpylib.source.current as current
-import magpylib.source.moment as moment
+import magpylib._lib.classes.magnet as magnet
+import magpylib._lib.classes.current as current
+import magpylib._lib.classes.moment as moment
