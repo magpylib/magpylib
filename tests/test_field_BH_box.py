601b25d32b77345d7105eb6b879c727a0d809e29
Michael Ortner <magpylib@gmail.com>
2021-02-01 06:45:14 +0100

    adding BH_box test

--- tests/test_field_BH_box.py
+++ tests/test_field_BH_box.py
@@ -4,10 +4,11 @@ from magpylib3._lib.fields.field_BH_box import field_B_box
 def test_field_B_box():
     
     # special case test ---------------------------------------------
-    # corners
+    # all solutions at corners and edges should be zero
+    # corner positions
     pos = [(1,1,1), (1,1,-1), (1,-1,1), (-1,1,1),
             (1,-1,-1), (-1,1,-1), (-1,-1,1), (-1,-1,-1)]
-    # edges
+    # random edge positions
     pos += [(1,1,.2), (1,-1,.3), (-1,1,.4), (-1,-1,-.3)]
     pos += [(1,.5,1), (1,.6,-1), (-1,.7,1), (-1,.8,-1)]
     pos += [(-.2,1,1), (-.3,1,-1), (-.4,-1,1), (-.5,-1,-1)]
@@ -22,6 +23,7 @@ def test_field_B_box():
 
 
     # general case test ---------------------------------------------
+    # load random positions and solutions and compare
     mag, dim, pos, B = np.load('tests/testdata/testdata_field_B_box.npy')
 
     Btest = field_B_box(mag, dim, pos)
