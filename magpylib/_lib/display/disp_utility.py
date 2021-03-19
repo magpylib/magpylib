dd5914174358a9101373a23087aee7f72b5b491e
Michael Ortner <magpylib@gmail.com>
2021-03-19 12:23:29 +0100

    object representation, reduce code size

--- magpylib/_lib/display/disp_utility.py
+++ magpylib/_lib/display/disp_utility.py
@@ -39,19 +39,36 @@ def faces_box(src, show_path):
 
 def faces_cylinder(src, show_path):
     """
-    compute vertices and faces of cylinder input for plotting
-    takes Cylinder source,
-    returns vert, faces
-    returns all faces when show_path=all
+    Compute vertices and faces of Sphere input for plotting.
+
+    Parameters
+    ----------
+    - src (source object)
+    - show_path (bool or int)
+
+    Returns
+    -------
+    vert, faces (returns all faces when show_path=int)
     """
     # pylint: disable=protected-access
-    res = 20 # resolution
-    d,h = src.dim
-    phis = np.linspace(0,2*np.pi,res)
-    vert_circ = np.array([np.cos(phis),np.sin(phis),np.zeros(res)]).T*d/2
-    v_t0 = vert_circ + np.array([0,0,h/2]) # top vertices
-    v_b0 = vert_circ - np.array([0,0,h/2]) # bott vertices
+    res = 15 #surface discretization
 
+    # generate cylinder faces
+    r,h2 = src.dim/2
+    hs = np.array([-h2,h2])
+    phis = np.linspace(0,2*np.pi,res)
+    phis2 = np.roll(np.linspace(0,2*np.pi,res),1)
+    faces = [r*np.array([
+        (np.cos(p1), np.sin(p1),  h),
+        (np.cos(p1), np.sin(p1), -h),
+        (np.cos(p2), np.sin(p2), -h),
+        (np.cos(p2), np.sin(p2),  h)])
+        for p1,p2 in zip(phis,phis2) for h in hs]
+    faces += [r*np.array([
+        (np.cos(phi), np.sin(phi), h)
+         for phi in phis]) for h in hs]
+
+    # add src attributes position and orientation depending on show_path
     if not isinstance(show_path, bool) and src._pos.ndim>1:
         rots = src._rot[::-show_path]
         poss = src._pos[::-show_path]
@@ -59,13 +76,13 @@ def faces_cylinder(src, show_path):
         rots = [src._rot[-1]]
         poss = [src._pos[-1]]
 
-    faces = []
+    # all faces (incl. along path) adding pos and rot
+    all_faces = []
     for rot,pos in zip(rots,poss):
-        v_t = rot.apply(v_t0) + pos
-        v_b = rot.apply(v_b0) + pos
-        faces += [[v_t[i], v_b[i], v_b[i+1], v_t[i+1]] for i in range(res-1)]
-        faces += [v_t, v_b]
-    return faces
+        for face in faces:
+            all_faces += [[rot.apply(f) + pos for f in face]]
+
+    return all_faces
 
 
 def faces_sphere(src, show_path):
@@ -82,27 +99,22 @@ def faces_sphere(src, show_path):
     vert, faces (returns all faces when show_path=int)
     """
     # pylint: disable=protected-access
+    res = 15 #surface discretization
 
-    # generate faces
+    # generate sphere faces
     r = src.dim/2
-    Ns = [4,8,16,16,16,16,16,16,16,8,4]
-    us = np.linspace(np.pi/2,-np.pi/2,len(Ns)+2)[1:-1]
-
-    t4 = np.linspace(0,2*np.pi,5)
-    u0,u1 = us[0], us[-1]
-    f_top = np.array([(r*cos(t)*cos(u0), r*sin(t)*cos(u0), r*sin(u0)) for t in t4])
-    f_bot = np.array([(r*cos(t)*cos(u1), r*sin(t)*cos(u1), r*sin(u1)) for t in t4])
-    faces = [f_top,f_bot]
-
-    shift = 1
-    for i,N in enumerate(Ns[:-1]):
-        if Ns[i+1]>N:
-            faces += faces_sphere_add_incr(r,us[i],us[i+1],N)
-        elif Ns[i+1]==N:
-            faces += faces_sphere_add_keep(r,us[i],us[i+1],N,shift)
-            shift *= -1
-        else:
-            faces += faces_sphere_add_incr(r,us[i+1],us[i],Ns[i+1])
+    phis = np.linspace(0,2*np.pi,res)
+    phis2 = np.roll(np.linspace(0,2*np.pi,res),1)
+    ths = np.linspace(0,np.pi,res)
+    faces = [r*np.array([
+            (cos(p)*sin(t1), sin(p)*sin(t1), cos(t1)),
+            (cos(p)*sin(t2), sin(p)*sin(t2), cos(t2)),
+            (cos(p2)*sin(t2), sin(p2)*sin(t2), cos(t2)),
+            (cos(p2)*sin(t1), sin(p2)*sin(t1), cos(t1))])
+            for p,p2 in zip(phis,phis2) for t1,t2 in zip(ths[1:-2],ths[2:-1])]
+    faces += [r*np.array([
+            (cos(p)*sin(th), sin(p)*sin(th), cos(th))
+            for p in phis]) for th in [ths[1],ths[-2]]]
 
     # add src attributes position and orientation depending on show_path
     if not isinstance(show_path, bool) and src._pos.ndim>1:
@@ -112,65 +124,13 @@ def faces_sphere(src, show_path):
         rots = [src._rot[-1]]
         poss = [src._pos[-1]]
 
+    # all faces (incl. along path) adding pos and rot
     all_faces = []
     for rot,pos in zip(rots,poss):
         for face in faces:
             all_faces += [[rot.apply(f) + pos for f in face]]
 
     return all_faces
-
-
-def faces_sphere_add_incr(r,u1,u2,N):
-    """
-    add a row of faces to sphere surface with increasing discretization
-
-    Parameters
-    ---------
-    - r (float): radius
-    - u1 (float): first polar angle
-    - u2 (float): second polar angle
-    - N (int): discretization
-
-    Returns
-    -------
-    list of faces that are ndarray (N,3)
-    """
-    t1 = np.linspace(0,4*np.pi,2*N+1)
-    t2 = np.linspace(0,4*np.pi,4*N+1)+np.pi/N
-    v1 = r*np.array([(np.cos(t)*np.cos(u1), np.sin(t)*np.cos(u1), np.sin(u1)) for t in t1])
-    v2 = r*np.array([(np.cos(t)*np.cos(u2), np.sin(t)*np.cos(u2), np.sin(u2)) for t in t2])
-    faces = []
-    for i in range(N):
-        faces += [np.array([v1[i], v1[i+1], v2[2*i]])]
-        faces += [np.array([v1[i+1], v2[2*i], v2[2*i+1]])]
-        faces += [np.array([v1[i+1], v2[2*i+1], v2[2*i+2]])]
-    return faces
-
-
-def faces_sphere_add_keep(r,u1,u2,N,shift):
-    """
-    add a row of faces to sphere surface while keeping discretization
-
-    Parameters
-    ---------
-    - r (float): radius
-    - u1 (float): first polar angle
-    - u2 (float): second polar angle
-    - N (int): discretization
-
-    Returns
-    -------
-    list of faces that are ndarray (N,3)
-    """
-    t1 = np.linspace(0,4*np.pi,2*N+1) - (1-shift)*np.pi/N/2
-    t2 = np.linspace(0,4*np.pi,2*N+1) + (1+shift)*np.pi/N/2
-    v1 = r*np.array([(np.cos(t)*np.cos(u1), np.sin(t)*np.cos(u1), np.sin(u1)) for t in t1])
-    v2 = r*np.array([(np.cos(t)*np.cos(u2), np.sin(t)*np.cos(u2), np.sin(u2)) for t in t2])
-    faces = []
-    for i in range(N):
-        faces += [np.array([v1[i], v1[i+1], v2[i]])]
-        faces += [np.array([v1[i+1], v2[i], v2[i+1]])]
-    return faces
 
 
 def system_size(face_points, pix_points, markers, path_points):
