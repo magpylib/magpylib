58e24fe24fb7b694439e3d810271413fb6d641fd
Alexandre Boisselet <alexabois@gmail.com>
2021-11-10 21:41:49 +0100

    sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -123,14 +123,18 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
+
 html_logo = './_static/images/logo_rtd3.jpg'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
-
+html_theme_options = {
+    "repository_url": "https://github.com/magpylib/magpylib/tree/master/docs",
+    "use_repository_button": True,
+    "use_download_button": True,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
