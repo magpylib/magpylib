97d9abb89c46a9dce56560b683947773c46a99d6
Alexandre Boisselet <alexabois@gmail.com>
2021-11-16 01:04:46 +0100

    update examples

--- examples/plot_03_display_styling.py
+++ examples/plot_03_display_styling.py
@@ -4,37 +4,101 @@ Display styling
 =================
 """
 
-#%%
+# %%
 # Displaying objects may not yield by default the visual representation the user wants. For this cases, the library includes
-# a variety of styling options that can be applied at multiple levels in the user's code. 
-#  
-# .. warning::
-#   Even if both backends can display all object of the library, there is no 100% feature parity between
-#   them. Some of the differences include (non-exhaustive list):
-#   
-#   - ``magnetization.size`` -> ``matplotlib`` only
-#   - ``magnetization.color`` -> ``plotly`` only
-
+# a variety of styling options that can be applied at multiple levels in the user's code.
 
 # %%
-# List of available styles
-import magpylib as magpy
-from magpylib._src.default_utils import linearize_dict, get_defaults_dict
-style = get_defaults_dict('display.style')
-lin_style = linearize_dict(style)
-print('\n'.join(f"{k!r}: {v!r}" for k,v in lin_style.items()))
+# Hierarchy of arguments
+# ----------------------
+# from LOWEST to HIGHEST precedence
+#
+# - library ``defaults``
+# - individual object ``style`` or at ``Collection`` level
+# - in the ``display`` function
+
+# %%
+# Examples
+# --------
 
 #%%
-# Display figure on your own canvas (here Matplotlib 3D axis):
-
-import plotly.graph_objects as go
 import magpylib as magpy
+import plotly.graph_objects as go
+
+magpy.defaults.reset()
+
+cuboid = magpy.magnet.Cuboid(
+    magnetization=(1, 0, 0), dimension=(1, 1, 1), position=(0, 0, 0)
+)
+cylinder = magpy.magnet.Cylinder(
+    magnetization=(0, 1, 0), dimension=(1, 1), position=(2, 0, 0)
+)
+sphere = magpy.magnet.Sphere(magnetization=(0, 1, 1), diameter=1, position=(4, 0, 0))
+col = magpy.Collection(cuboid, cylinder, sphere)
+
+#%%
+# **Setting library defaults**:
+my_default_magnetization_style = {
+    "show": True,
+    "color": {
+        "transition": 1,
+        "mode": "tricolor",
+        "middle": "white",
+        "north": "magenta",
+        "south": "turquoise",
+    },
+}
+magpy.defaults.display.style.magnet.magnetization = my_default_magnetization_style
 
 fig = go.Figure()
-src = magpy.magnet.Sphere(magnetization=(0, 0, 1), diameter=1)
-src.move([(0.1, 0, 0)] * 50, increment=True)
-src.rotate_from_angax(angle=[10] * 50, axis="z", anchor=0, start=0, increment=True)
-ts = [-0.4, 0, 0.4]
-sens = magpy.Sensor(position=(0, 0, 2), pixel=[(x, y, 0) for x in ts for y in ts])
-magpy.display(src, sens, canvas=fig, backend="plotly")
+magpy.display(col, canvas=fig, backend="plotly")
 fig
+
+#%%
+# **Setting style via ``Collection``**:
+#
+# .. note::
+#   The ``Collection`` object does not hold any ``style`` attribute on its own but the helper method ``set_styles``
+#   allows setting the styles of all its children where the set arguments match existing child style attributes.
+
+col.set_styles(magnetization_color_south='blue')
+
+fig = go.Figure()
+magpy.display(col, canvas=fig, backend="plotly")
+fig
+
+#%%
+# **Setting individual styles**:
+cylinder.style.update(magnetization_color_mode = 'bicolor')
+cuboid.style.magnetization.color = dict(mode = 'tricycle')
+
+fig = go.Figure()
+magpy.display(col, canvas=fig, backend="plotly")
+fig
+
+#%%
+# **Overriding style at display time**:
+#
+# .. note::
+#   Setting style parameters in the ``display`` function does not change the default styles nor the set object style. It only
+#   affects the current representation to be displayed.
+#
+# The provided styling properties as function arguments will override temporarily the styles set by any of the afermentioned methods.
+# All styling properties need to start with ``style`` and underscore magic is supported.
+
+fig = go.Figure()
+magpy.display(col, canvas=fig, backend="plotly", style_magnetization_show=False)
+fig
+# %%
+# List of available styles
+# ------------------------
+style = magpy.defaults.display.style.as_dict(flatten=True)
+print("\n".join(f"{k!r}: {v!r}" for k, v in style.items()))
+
+# %%
+# .. warning::
+#   Even if both ``matplotlib`` and ``plotly`` backends can display all object of the library, there is no 100% feature parity between
+#   them. Some of the differences include (non-exhaustive list):
+#
+#   - ``magnetization.size`` -> ``matplotlib`` only
+#   - ``magnetization.color`` -> ``plotly`` only
