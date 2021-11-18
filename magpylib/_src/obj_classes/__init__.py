1d1a04fdd327f014dcce85c3fb1870b513b3392f
Alexandre Boisselet <alexabois@gmail.com>
2021-11-19 00:55:18 +0100

    fix CustomSource renaming

--- magpylib/_src/obj_classes/__init__.py
+++ magpylib/_src/obj_classes/__init__.py
@@ -1,7 +1,7 @@
 """_src.obj_classes"""
 
 __all__ = ['Cuboid', 'Cylinder', 'Sphere', 'Collection', 'Sensor',
-    'Dipole', 'Loop', 'Line', 'CylinderSegment','Custom']
+    'Dipole', 'Loop', 'Line', 'CylinderSegment','CustomSource']
 
 # create interface to outside of package
 from magpylib._src.obj_classes.class_mag_Cuboid import Cuboid
