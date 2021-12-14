212918bf95518522baa7de54818f1b425693f763
Alexandre Boisselet <alexabois@gmail.com>
2021-12-14 16:34:24 +0100

    Line direction bug (#430)
    * draw_arrow -> draw_arrowed_line
    
    * fix arrow direction when vertex is in -y direction
    
    * add test
    
    Co-authored-by: Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>

--- magpylib/_src/display/disp_utility.py
+++ magpylib/_src/display/disp_utility.py
@@ -31,15 +31,22 @@ def place_and_orient_model3d(
     return {**model_dict, xyz[0]: x, xyz[1]: y, xyz[2]: z, **kwargs}
 
 
-def draw_arrow(vec, pos, sign=1, arrow_size=1) -> Tuple:
+def draw_arrowed_line(vec, pos, sign=1, arrow_size=1) -> Tuple:
     """
     Provides x,y,z coordinates of an arrow drawn in the x-y-plane (z=0), showing up the y-axis and
     centered in x,y,z=(0,0,0). The arrow vertices are then turned in the direction of `vec` and
     moved to position `pos`.
     """
+    norm = np.linalg.norm(vec)
+    nvec = np.array(vec) / norm
+    yaxis = np.array([0, 1, 0])
+    cross = np.cross(nvec, yaxis)
+    dot = np.dot(nvec, yaxis)
+    n = np.linalg.norm(cross)
+    if dot==-1:
+        sign *= -1
     hy = sign * 0.1 * arrow_size
     hx = 0.06 * arrow_size
-    norm = np.linalg.norm(vec)
     arrow = (
         np.array(
             [
@@ -54,11 +61,6 @@ def draw_arrow(vec, pos, sign=1, arrow_size=1) -> Tuple:
         )
         * norm
     )
-    nvec = np.array(vec) / norm
-    yaxis = np.array([0, 1, 0])
-    cross = np.cross(nvec, yaxis)
-    dot = np.dot(nvec, yaxis)
-    n = np.linalg.norm(cross)
     if n != 0:
         t = np.arccos(dot)
         R = RotScipy.from_rotvec(-t * cross / n)
@@ -73,7 +75,7 @@ def draw_arrow_from_vertices(vertices, current, arrow_size):
     positions = vertices[:-1] + vectors / 2
     vertices = np.concatenate(
         [
-            draw_arrow(vec, pos, np.sign(current), arrow_size=arrow_size)
+            draw_arrowed_line(vec, pos, np.sign(current), arrow_size=arrow_size)
             for vec, pos in zip(vectors, positions)
         ],
         axis=1,
