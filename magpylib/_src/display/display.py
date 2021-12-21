9110627f3804602fb07c30db21834947feb9e9da
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2021-12-21 13:27:21 +0100

    include mag arrows in sys size calc

--- magpylib/_src/display/display.py
+++ magpylib/_src/display/display.py
@@ -280,9 +280,10 @@ def display_matplotlib(
                 points += [np.vstack(pts).reshape(-1, 3)]
                 if style.magnetization.show:
                     check_excitations([obj])
-                    draw_directs_faced(
+                    pts = draw_directs_faced(
                         [obj], [color], ax, show_path, style.magnetization.size
                     )
+                    points += pts
         if show_path:
             marker, line = style.path.marker, style.path.line
             points += draw_path(
