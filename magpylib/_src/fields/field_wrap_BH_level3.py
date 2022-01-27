4d96521eb360e5a8e7a7725b3188460364105eaa
Alexandre Boisselet <alexabois@gmail.com>
2022-01-27 08:20:35 +0100

    pylint

--- magpylib/_src/fields/field_wrap_BH_level3.py
+++ magpylib/_src/fields/field_wrap_BH_level3.py
@@ -142,10 +142,10 @@ def getB(sources=None, observers=None, sumup=False, squeeze=True, **kwargs):
 def getH(sources=None, observers=None, sumup=False, squeeze=True, **kwargs):
     """
     Compute H-field in [kA/m] for given sources and observers.
-    
+
     - Object-oriented (default):
         ``sources`` are previously defined Magpylib objects or list thereof
-    
+
     - Direct interface:
         Field implementations can be directly accessed for faster computation. Note that ``sources``
         parameter only accepts a single source and corresponding input parameters must be defined
