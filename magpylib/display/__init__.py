19868e988a77d6639ead859eae6765c4808b7281
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-01-28 15:07:39 +0100

    pylint

--- magpylib/display/__init__.py
+++ magpylib/display/__init__.py
@@ -3,5 +3,5 @@
 """
 display package
 
-This package regroups additional display features that can be used independently from the main package
+This package regroups additional display features that can be used independently.
 """
