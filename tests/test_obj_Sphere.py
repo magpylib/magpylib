092b14a317119934e88ec40dafae7d5029ecf57d
Michael Ortner <magpylib@gmail.com>
2022-01-31 13:16:37 +0100

    fix sphere_basic test (negative input)

--- tests/test_obj_Sphere.py
+++ tests/test_obj_Sphere.py
@@ -5,33 +5,33 @@ from magpylib.magnet import Sphere
 from magpylib import Collection
 import magpylib as magpy
 
-# """data generation for test_Sphere()"""
+# # """data generation for test_Sphere()"""
 
-# N = 100
+# # N = 100
 
-# mags = (np.random.rand(N,3)-0.5)*1000
-# dims = (np.random.rand(N)-0.5)*5
-# posos = (np.random.rand(N,333,3)-0.5)*10 #readout at 333 positions
+# # mags = (np.random.rand(N,3)-0.5)*1000
+# # dims = np.random.rand(N)*5
+# # posos = (np.random.rand(N,333,3)-0.5)*10 #readout at 333 positions
 
-# angs =  (np.random.rand(N,18)-0.5)*2*10 # each step rote by max 10 deg
-# axs =   (np.random.rand(N,18,3)-0.5)
-# anchs = (np.random.rand(N,18,3)-0.5)*5.5
-# movs =  (np.random.rand(N,18,3)-0.5)*0.5
+# # angs =  (np.random.rand(N,18)-0.5)*2*10 # each step rote by max 10 deg
+# # axs =   (np.random.rand(N,18,3)-0.5)
+# # anchs = (np.random.rand(N,18,3)-0.5)*5.5
+# # movs =  (np.random.rand(N,18,3)-0.5)*0.5
 
-# B = []
-# for mag,dim,ang,ax,anch,mov,poso in zip(mags,dims,angs,axs,anchs,movs,posos):
-#     pm = Sphere(mag,dim)
+# # B = []
+# # for mag,dim,ang,ax,anch,mov,poso in zip(mags,dims,angs,axs,anchs,movs,posos):
+# #     pm = magpy.magnet.Sphere(mag,dim)
 
-#     # 18 subsequent operations
-#     for a,aa,aaa,mv in zip(ang,ax,anch,mov):
-#         pm.move(mv).rotate_from_angax(a,aa,aaa)
+# #     # 18 subsequent operations
+# #     for a,aa,aaa,mv in zip(ang,ax,anch,mov):
+# #         pm.move(mv).rotate_from_angax(a,aa,aaa)
 
-#     B += [pm.getB(poso)]
-# B = np.array(B)
+# #     B += [pm.getB(poso)]
+# # B = np.array(B)
 
-# inp = [mags,dims,posos,angs,axs,anchs,movs,B]
+# # inp = [mags,dims,posos,angs,axs,anchs,movs,B]
 
-# pickle.dump(inp,open('testdata_Sphere.p', 'wb'))
+# # pickle.dump(inp, open('testdata_Sphere.p', 'wb'))
 
 def test_Sphere_basics():
     """ test Cuboid fundamentals, test against magpylib2 fields
