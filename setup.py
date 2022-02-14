fa3527733929b08070d5fb1a3f461121e925d2bd
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-02-14 09:48:57 +0100

    add python 3.9 and 3.10 to classifiers

--- setup.py
+++ setup.py
@@ -82,9 +82,11 @@ setuptools.setup(
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
