8ea649c39ebb35839e7bc42ee4080bb62c3f71b7
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-02-17 18:14:56 +0100

    add param dependency

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     zip_safe=False,  ## Gives the environment files so we can access docs,
     ## enables tooltips but may decrease performance
-    install_requires=["numpy>=1.20", "scipy>=1.7", "matplotlib>=3.3",],
+    install_requires=["numpy>=1.20", "scipy>=1.7", "matplotlib>=3.3", "param>=1.12"],
     # kaleido, jupyterlab are needed for testing with display(renderer='json', backend='plotly')
     extras_require={
         "testing": [
