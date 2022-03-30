30df48368b1f5b4866d3629c2181b93cfeaa5948
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-03-30 12:11:25 +0200

    refactor getBHdict

--- magpylib/_src/fields/field_wrap_BH_level2_dict.py
+++ magpylib/_src/fields/field_wrap_BH_level2_dict.py
@@ -6,7 +6,31 @@ from magpylib._src.fields.field_wrap_BH_level1 import getBH_level1
 from magpylib._src.exceptions import MagpylibBadUserInput
 from magpylib._src.utility import LIBRARY_BH_DICT_SOURCE_STRINGS
 
-def getBH_dict_level2(squeeze=True, **kwargs: dict) -> np.ndarray:
+
+PARAM_TILE_DIMS = {
+    "observer": 2,
+    "position": 2,
+    "orientation": 2,
+    "magnetization": 2,
+    "current": 1,
+    "moment": 2,
+    "dimension": 2,
+    "diameter": 1,
+    "segment_start": 2,
+    "segment_end": 2,
+}
+
+
+def getBH_dict_level2(
+    source_type,
+    observer,
+    *,
+    field: str,
+    position=(0, 0, 0),
+    orientation=R.identity(),
+    squeeze=True,
+    **kwargs: dict,
+) -> np.ndarray:
     """ Direct interface access to vectorized computation
 
     Parameters
@@ -35,106 +59,57 @@ def getBH_dict_level2(squeeze=True, **kwargs: dict) -> np.ndarray:
     #  getBH_level1().
     #  To allow different input dimensions, the tdim argument is also given
     #  which tells the program which dimension it should tile up.
-    tile_params = {}
 
-    # mandatory general inputs ------------------
-    try:
-        src_type = kwargs['source_type']
-        if src_type not in LIBRARY_BH_DICT_SOURCE_STRINGS:
-            raise MagpylibBadUserInput(
-                f"Input parameter `sources` must be one of {LIBRARY_BH_DICT_SOURCE_STRINGS}"
-                " when using the direct interface.")
-
-        poso = np.array(kwargs['observer'], dtype=float)
-        tile_params['observer'] = (poso,2)    # <-- (input,tdim)
-
-        # optional general inputs -------------------
-        # if no input set pos=0
-        pos = np.array(kwargs.get('position', (0,0,0)), dtype=float)
-        tile_params['position'] = (pos,2)
-        # if no input set rot=unit
-        rot = kwargs.get('orientation', R.from_quat((0,0,0,1)))
-        tile_params['orientation'] = (rot.as_quat(),2)
-
-        # mandatory class specific inputs -----------
-        if src_type == 'Cuboid':
-            mag = np.array(kwargs['magnetization'], dtype=float)
-            tile_params['magnetization'] = (mag,2)
-            dim = np.array(kwargs['dimension'], dtype=float)
-            tile_params['dimension'] = (dim,2)
-
-        elif src_type == 'Cylinder':
-            mag = np.array(kwargs['magnetization'], dtype=float)
-            tile_params['magnetization'] = (mag,2)
-            dim = np.array(kwargs['dimension'], dtype=float)
-            tile_params['dimension'] = (dim,2)
-
-        elif src_type == 'CylinderSegment':
-            mag = np.array(kwargs['magnetization'], dtype=float)
-            tile_params['magnetization'] = (mag,2)
-            dim = np.array(kwargs['dimension'], dtype=float)
-            tile_params['dimension'] = (dim,2)
-
-        elif src_type == 'Sphere':
-            mag = np.array(kwargs['magnetization'], dtype=float)
-            tile_params['magnetization'] = (mag,2)
-            dia = np.array(kwargs['diameter'], dtype=float)
-            tile_params['diameter'] = (dia,1)
-
-        elif src_type == 'Dipole':
-            moment = np.array(kwargs['moment'], dtype=float)
-            tile_params['moment'] = (moment,2)
-
-        elif src_type == 'Loop':
-            current = np.array(kwargs['current'], dtype=float)
-            tile_params['current'] = (current,1)
-            dia = np.array(kwargs['diameter'], dtype=float)
-            tile_params['diameter'] = (dia,1)
-
-        elif src_type == 'Line':
-            current = np.array(kwargs['current'], dtype=float)
-            tile_params['current'] = (current,1)
-            pos_start = np.array(kwargs['segment_start'], dtype=float)
-            tile_params['segment_start'] = (pos_start,2)
-            pos_end = np.array(kwargs['segment_end'], dtype=float)
-            tile_params['segment_end'] = (pos_end,2)
-
-    except KeyError as kerr:
+    if source_type not in LIBRARY_BH_DICT_SOURCE_STRINGS:
         raise MagpylibBadUserInput(
-            f"Missing input keys: {str(kerr)}"
-        ) from kerr
-    except TypeError as terr:
-        raise MagpylibBadUserInput(
-            "Bad user input type. When sources argument is a string,"
-            " all other inputs must be scalar or array_like."
-        ) from terr
-
-    # auto tile 1D parameters ---------------------------------------
-
-    # evaluation vector length
-    ns = [len(val) for val,tdim in tile_params.values() if val.ndim == tdim]
-    if len(set(ns)) > 1:
-        raise MagpylibBadUserInput(
-        "Input array lengths must be 1 or of a similarlength.\n"
-        f"Instead received {str(set(ns))}"
+            f"Input parameter `sources` must be one of {LIBRARY_BH_DICT_SOURCE_STRINGS}"
+            " when using the direct interface."
         )
-    n = max(ns, default=1)
+
+    kwargs["observer"] = observer
+    kwargs["position"] = position
+
+    # change orientation to Rotation numpy array for tiling
+    kwargs["orientation"] = orientation.as_quat()
+
+    # evaluation vector lengths
+    vec_lengths = []
+    for key, val in kwargs.items():
+        try:
+            val = np.array(val, dtype=float)
+        except TypeError as err:
+            raise MagpylibBadUserInput(
+                f"{key} input must be array-like.\n"
+                f"Instead received {val}"
+            ) from err
+        tdim = PARAM_TILE_DIMS.get(key, 1)
+        if val.ndim == tdim:
+            vec_lengths.append(len(val))
+        kwargs[key] = val
+
+    if len(set(vec_lengths)) > 1:
+        raise MagpylibBadUserInput(
+            "Input array lengths must be 1 or of a similar length.\n"
+            f"Instead received {set(vec_lengths)}"
+        )
+    vec_len = max(vec_lengths, default=1)
 
     # tile 1D inputs and replace original values in kwargs
-    for key,(val,tdim) in tile_params.items():
-        if val.ndim<tdim:
+    for key, val in kwargs.items():
+        tdim = PARAM_TILE_DIMS.get(key, 1)
+        if val.ndim < tdim:
             if tdim == 2:
-                kwargs[key] = np.tile(val,(n,1))
+                kwargs[key] = np.tile(val, (vec_len, 1))
             elif tdim == 1:
-                kwargs[key] = np.array([val]*n)
+                kwargs[key] = np.array([val] * vec_len)
         else:
             kwargs[key] = val
 
-    # change rot to Rotation object
-    kwargs['orientation'] = R.from_quat(kwargs['orientation'])
+    # change orientation back to Rotation object
+    kwargs["orientation"] = R.from_quat(kwargs["orientation"])
 
     # compute and return B
-    B = getBH_level1(**kwargs)
+    B = getBH_level1(source_type=source_type, field=field, **kwargs)
 
     if squeeze:
         return np.squeeze(B)
