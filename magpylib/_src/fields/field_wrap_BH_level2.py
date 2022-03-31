f07c1dcf275f756e683d4111badd6dd6ec0f5102
Alexandre Boisselet <alexabois@gmail.com>
2022-03-31 22:47:00 +0200

    fix pixel_agg

--- magpylib/_src/fields/field_wrap_BH_level2.py
+++ magpylib/_src/fields/field_wrap_BH_level2.py
@@ -156,7 +156,6 @@ def getBH_level2(
     #   allow only bare sensor, collection, pos_vec or list thereof
     #   transform input into an ordered list of sensors (pos_vec->pixel)
     #   check if all pixel shapes are similar - or else if pixel_agg is given
-    pixel_agg = kwargs.get("pixel_agg", None)
     pixel_agg_func = check_format_pixel_agg(pixel_agg)
     sensors, pix_shapes = check_format_input_observers(observers, pixel_agg)
     pix_nums = [
