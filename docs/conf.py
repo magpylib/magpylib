03fcdb85b70c21f5ae3dec23fbff2b1092efb0ac
Alexandre Boisselet <alexabois@gmail.com>
2022-04-14 21:44:39 +0200

    Pyvista dependencies (#522)
    * Enable Pyvista with read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,13 @@
 import os
 import sys
 
+# This is for pyvista
+os.system("/usr/bin/Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &")
+os.environ["DISPLAY"] = ":99"
+os.environ["PYVISTA_OFF_SCREEN"] = "true"
+os.environ["PYVISTA_USE_IPYVTK"] = "true"
+
+
 # Location of Sphinx files
 sys.path.insert(0, os.path.abspath("./../"))  ##Add the folder one level above
 os.environ[
