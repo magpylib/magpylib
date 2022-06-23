735d9053c3b9e194ce41f237b0eb54bb1cbf53f5
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-06-23 15:13:17 +0200

    remove pyvista (now generic branch)

--- magpylib/_src/style.py
+++ magpylib/_src/style.py
@@ -292,7 +292,7 @@ class Model3d(MagicProperties):
 
         backend: str
             Plotting backend corresponding to the trace. Can be one of
-            `['generic', 'matplotlib', 'plotly', 'pyvista']`.
+            `['generic', 'matplotlib', 'plotly']`.
 
         constructor: str
             Model constructor function or method to be called to build a 3D-model object
@@ -336,7 +336,7 @@ class Trace3d(MagicProperties):
     ----------
     backend: str
         Plotting backend corresponding to the trace. Can be one of
-        `['matplotlib', 'plotly', 'pyvista']`.
+        `['matplotlib', 'plotly']`.
 
     constructor: str
         Model constructor function or method to be called to build a 3D-model object
@@ -490,7 +490,7 @@ class Trace3d(MagicProperties):
     @property
     def backend(self):
         """Plotting backend corresponding to the trace. Can be one of
-        `['matplotlib', 'plotly', 'pyvista']`."""
+        `['matplotlib', 'plotly']`."""
         return self._backend
 
     @backend.setter
