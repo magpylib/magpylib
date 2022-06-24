0463120550121c2a05b7908f5aeb37381c6331aa
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-06-24 08:37:54 +0200

    simplify structure and avoid redundancy

--- magpylib/current/__init__.py
+++ magpylib/current/__init__.py
@@ -4,4 +4,5 @@ The `magpylib.current` subpackage contains all electric current classes.
 
 __all__ = ["Loop", "Line"]
 
-from magpylib._src.obj_classes import Loop, Line
+from magpylib._src.obj_classes.class_current_Loop import Loop
+from magpylib._src.obj_classes.class_current_Line import Line
--- magpylib/magnet/__init__.py
+++ magpylib/magnet/__init__.py
@@ -4,4 +4,8 @@ The `magpylib.magnet` subpackage contains all magnet classes.
 
 __all__ = ["Cuboid", "Cylinder", "Sphere", "CylinderSegment"]
 
-from magpylib._src.obj_classes import Cuboid, Cylinder, Sphere, CylinderSegment
+
+from magpylib._src.obj_classes.class_mag_Cuboid import Cuboid
+from magpylib._src.obj_classes.class_mag_Cylinder import Cylinder
+from magpylib._src.obj_classes.class_mag_Sphere import Sphere
+from magpylib._src.obj_classes.class_mag_CylinderSegment import CylinderSegment
