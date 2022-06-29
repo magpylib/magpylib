6ed44a8d983ee4b0242cd01d7a2cc0dc9c5af91c
Alexandre Boisselet <alexabois@gmail.com>
2022-06-29 23:21:28 +0200

    remove print statement

--- magpylib/_src/fields/field_BH_facet.py
+++ magpylib/_src/fields/field_BH_facet.py
@@ -154,7 +154,6 @@ def mask_inside_trimesh(points, facets):
 
     # choose a start point that is fore sure outside the mesh
     pts_ins_box = points[mask]
-    print(pts_ins_box.shape)
     start_point = np.min(vertices, axis=0) - 1.001
     segments = np.tile(start_point, (pts_ins_box.shape[0], 1))
     t = facets.swapaxes(0, 1)
