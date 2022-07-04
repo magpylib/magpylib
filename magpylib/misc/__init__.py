bd6f548405e24ad0be9e691478d68994631c73f0
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-07-04 20:02:51 +0200

    automatize imports

--- magpylib/misc/__init__.py
+++ magpylib/misc/__init__.py
@@ -1,8 +1,20 @@
 """
 The `magpylib.misc` sub-package contains miscellaneous source objects.
 """
+import importlib
+from pkgutil import iter_modules
 
-__all__ = ["Dipole", "CustomSource"]
+from magpylib._src import obj_classes
 
-from magpylib._src.obj_classes.class_misc_Dipole import Dipole
-from magpylib._src.obj_classes.class_misc_Custom import CustomSource
+classes = []
+
+for submodule in iter_modules(obj_classes.__path__):
+    if submodule.name.startswith("class_misc"):
+        _, typ, cls_name = submodule.name.split("_")
+        classes.append(cls_name)
+        module = importlib.import_module(
+            f"{obj_classes.__name__}.{submodule.name}", cls_name
+        )
+        vars()[cls_name] = getattr(module, cls_name)
+
+__all__ = classes
