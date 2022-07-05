1212aeadde780e60998375789007a2af8bb09a92
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2022-07-05 14:23:29 +0200

    general rework

--- magpylib/_src/display/backend_mayavi.py
+++ magpylib/_src/display/backend_mayavi.py
@@ -4,33 +4,11 @@ import numpy as np
 from matplotlib.colors import colorConverter
 from mayavi import mlab
 
-from magpylib._src.display.traces_generic import draw_frame
+from magpylib._src.display.traces_generic import get_frames
+from magpylib._src.display.traces_utility import subdivide_mesh_by_facecolor
 
 
 # from magpylib._src.utility import format_obj_input
-
-
-def subdivide_mesh_by_facecolor(trace):
-    """Subdivide a mesh into a list of meshes based on facecolor"""
-    # TODO so far the function keeps all x,y,z coords for all subtraces, which is convienient since
-    # it does not require to recalculate the indices i,j,k. If many different colors, this is
-    # become inpractical.
-    facecolor = trace["facecolor"]
-    subtraces = []
-    last_ind = 0
-    prev_color = facecolor[0]
-    # pylint: disable=singleton-comparison
-    facecolor[facecolor == None] = "black"
-    for ind, color in enumerate(facecolor):
-        if color != prev_color or ind == len(facecolor) - 1:
-            new_trace = trace.copy()
-            for k in "ijk":
-                new_trace[k] = trace[k][last_ind:ind]
-            new_trace["color"] = prev_color
-            last_ind = ind
-            prev_color = color
-            subtraces.append(new_trace)
-    return subtraces
 
 
 def generic_trace_to_mayavi(trace):
@@ -89,69 +67,44 @@ def display_mayavi(
     **kwargs,
 ):
 
-    """
-    Display objects and paths graphically using the mayavi library.
-
-    Parameters
-    ----------
-    objects: sources, collections or sensors
-        Objects to be displayed.
-
-    markers: array_like, None, shape (N,3), default=None
-        Display position markers in the global CS. By default no marker is displayed.
-
-    zoom: float, default = 1
-        Adjust plot zoom-level. When zoom=0 all objects are just inside the 3D-axes.
-
-    canvas: #TODO what canvas
-        Display graphical output in a given canvas
-        By default a new `Figure` is created and displayed.
-
-    title: str, default = "3D-Paths Animation"
-        When zoom=0 all objects are just inside the 3D-axes.
-
-    colorsequence: list or array_like, iterable, default=
-            ['#2E91E5', '#E15F99', '#1CA71C', '#FB0D0D', '#DA16FF', '#222A2A',
-            '#B68100', '#750D86', '#EB663B', '#511CFB', '#00A08B', '#FB00D1',
-            '#FC0080', '#B2828D', '#6C7C32', '#778AAE', '#862A16', '#A777F1',
-            '#620042', '#1616A7', '#DA60CA', '#6C4516', '#0D2A63', '#AF0038']
-        An iterable of color values used to cycle trough for every object displayed.
-        A color and may be specified as:
-      - A hex string (e.g. '#ff0000')
-      - An rgb/rgba string (e.g. 'rgb(255,0,0)')
-      - An hsl/hsla string (e.g. 'hsl(0,100%,50%)')
-      - An hsv/hsva string (e.g. 'hsv(0,100%,100%)')
-      - A named CSS color
-    """
+    """Display objects and paths graphically using the mayavi library."""
 
     if animation is not False:
         msg = "The mayavi backend does not support animation at the moment.\n"
         msg += "Use `backend=plotly` instead."
         warnings.warn(msg)
-        # animation = False
+        animation = False
 
     # flat_obj_list = format_obj_input(obj_list)
 
-    show_canvas = False
-    if canvas is None:
-        show_canvas = True
-        canvas = mlab
+    show_canvas = True
+    if canvas == "hold":
+        show_canvas = False
+    elif canvas is not None:
+        msg = (
+            "The mayavi backend does not support a specific backend. You can specify "
+            "`canvas='hold'` if you want to hold `on mlab.show()`"
+        )
+        warnings.warn(msg)
 
-    generic_traces = draw_frame(
-        obj_list,
-        colorsequence,
-        zoom,
-        output="list",
+    data = get_frames(
+        objs=obj_list,
+        colorsequence=colorsequence,
+        zoom=zoom,
+        animation=animation,
+        extra_backend="pyvista",
         mag_arrows=True,
         **kwargs,
     )
-    for tr in generic_traces:
-        for tr1 in generic_trace_to_mayavi(tr):
+
+    frame = data["frames"][0]  # select first, since no animation supported
+
+    for tr0 in frame["data"]:
+        for tr1 in generic_trace_to_mayavi(tr0):
             constructor = tr1["constructor"]
             args = tr1["args"]
             kwargs = tr1["kwargs"]
-            getattr(canvas, constructor)(*args, **kwargs)
+            getattr(mlab, constructor)(*args, **kwargs)
 
-    # apply_fig_ranges(canvas, zoom=zoom)
     if show_canvas:
         mlab.show()
