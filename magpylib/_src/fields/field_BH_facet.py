50b3148dc5446660b27f17a5303c8ffbe38afab6
Michael Orter <ortner@research.silicon-austria.com>
2022-11-03 22:38:15 +0100

    facet core small improvements

--- magpylib/_src/fields/field_BH_facet.py
+++ magpylib/_src/fields/field_BH_facet.py
@@ -3,8 +3,7 @@ Implementations of analytical expressions for the magnetic field of a triangular
 Computation details in function docstrings.
 """
 import numpy as np
-from scipy.spatial import ConvexHull  # pylint: disable=no-name-in-module
-
+#from scipy.spatial import ConvexHull  # pylint: disable=no-name-in-module
 from magpylib._src.input_checks import check_field_input
 
 ###########
@@ -29,18 +28,12 @@ def norm_vector(v):
     return n / np.expand_dims(n_norm, axis=-1)
 
 
-def mydot(a, b):
-    """
-    Vectorized 3d dot-product.
-    """
-    return a[:, 0] * b[:, 0] + a[:, 1] * b[:, 1] + a[:, 2] * b[:, 2]
-
-
 def scalar3(R1, R2, R3):
     """
     Calculates (oriented) volume of the parallelepiped defined by the vectors R1, R2, R3
+     = R1 * R2 x R3
     """
-    return mydot(R1, np.cross(R2, R3))
+    return np.einsum("ij, ij->i", R1, np.cross(R2, R3))
 
 
 def solid_angle(R1, R2, R3, r1, r2, r3):
@@ -48,133 +41,24 @@ def solid_angle(R1, R2, R3, r1, r2, r3):
     Calculates the solid angle of the triangle defined by the position vectors R1, R2, R3
     """
     N = scalar3(R1, R2, R3)
-    D = r1 * r2 * r3 + mydot(R1, R2) * r3 + mydot(R1, R3) * r2 + mydot(R2, R3) * r1
+    D = (r1 * r2 * r3 +
+        np.einsum("ij, ij->i", R1, R2) * r3 +
+        np.einsum("ij, ij->i", R1, R3) * r2 +
+        np.einsum("ij, ij->i", R2, R3) * r1)
     return 2.0 * np.arctan2(N, D)
 
 
-def next_i(i):
-    """
-    Returns next index modulo 3
-    """
-    if i == 2:
-        return 0
-    return i + 1
-
-
-################
-# mask functions
-################
-
-
-def mask_inside_enclosing_box(points, vertices, tol=1e-15):
-    """Return a mask for `points` which truth value tells if inside the
-    bounding box"""
-    xmin, ymin, zmin = np.min(vertices, axis=0)
-    xmax, ymax, zmax = np.max(vertices, axis=0)
-    x, y, z = points.T
-    # within cuboid dimension with positive tolerance
-    mx = (x - xmax < tol) & (xmin - x < tol)
-    my = (y - ymax < tol) & (ymin - y < tol)
-    mz = (z - zmax < tol) & (zmin - z < tol)
-    return mx & my & mz
-
-
-def mask_inside_facets_convexhull(points, vertices):
-    """Return a mask for `points` which truth value tells if inside the
-    convexhulls built from provided `vertices`."""
-    # check first if points are in enclosing box, to save costly convexhull computation
-    inside_enclosing_box = mask_inside_enclosing_box(points, vertices)
-    hull = ConvexHull(vertices, incremental=True)
-    simplices_init = hull.simplices
-    for ind, pt in enumerate(points):
-        if inside_enclosing_box[ind]:
-            hull.add_points([pt])
-            if hull.simplices.shape != simplices_init.shape or not np.all(
-                hull.simplices == simplices_init
-            ):
-                inside_enclosing_box[ind] = False
-                hull.close()
-                hull = ConvexHull(vertices, incremental=True)
-    hull.close()
-    return inside_enclosing_box
-
-
-def signed_volume(a, b, c, d):
-    """Computes the signed volume of a series of tetrahedrons defined by the vertices in
-    a, b c and d. The ouput is an SxT array which gives the signed volume of the tetrahedron
-    defined by the line segment 's' and two vertices of the triangle 't'."""
-
-    return np.sum((a - d) * np.cross(b - d, c - d), axis=2)
-
-
-def segments_intersect_triangles(s, t):
-    """For each line segment in 's', this function computes how many times it intersects
-    any of the triangles given in 't'."""
-    # compute the normals to each triangle
-    normals = np.cross(t[2] - t[0], t[2] - t[1])
-    normals /= np.linalg.norm(normals, axis=1)[:, np.newaxis]
-
-    # get sign of each segment endpoint, if the sign changes then we know this segment crosses the
-    # plane which contains a triangle. If the value is zero the endpoint of the segment lies on the
-    # plane.
-    # s[i][:, np.newaxis] - t[j] -> S x T x 3 array
-    sign1 = np.sign(np.sum(normals * (s[0][:, np.newaxis] - t[2]), axis=2))  # S x T
-    sign2 = np.sign(np.sum(normals * (s[1][:, np.newaxis] - t[2]), axis=2))  # S x T
-
-    # determine segments which cross the plane of a triangle.
-    #  -> 1 if the sign of the end points of s is
-    # different AND one of end points of s is not a vertex of t
-    cross = (sign1 != sign2) * (sign1 != 0) * (sign2 != 0)  # S x T
-
-    # get signed volumes
-    v1 = np.sign(
-        signed_volume(t[0], t[1], s[0][:, np.newaxis], s[1][:, np.newaxis])
-    )  # S x T
-    v2 = np.sign(
-        signed_volume(t[1], t[2], s[0][:, np.newaxis], s[1][:, np.newaxis])
-    )  # S x T
-    v3 = np.sign(
-        signed_volume(t[2], t[0], s[0][:, np.newaxis], s[1][:, np.newaxis])
-    )  # S x T
-
-    same_volume = np.logical_and(
-        (v1 == v2), (v2 == v3)
-    )  # 1 if s and t have same sign in v1, v2 and v3
-
-    return np.sum(cross * same_volume, axis=1)
-
-
-def mask_inside_trimesh(points, facets):
-    """Return a boolean mask corresponding to the truth values of which points are inside
-    the triangular mesh defined by the provided facets, using the ray tracing method"""
-    # compute only points outside enclosing box more efficiently
-    vertices = np.unique(facets.reshape((-1, 3)), axis=0)
-    mask = mask_inside_enclosing_box(points, vertices)
-
-    # choose a start point that is fore sure outside the mesh
-    pts_ins_box = points[mask]
-    start_point = np.min(vertices, axis=0) - np.array([1.001, 0.992, 0.993])
-    segments = np.tile(start_point, (pts_ins_box.shape[0], 1))
-    t = facets.swapaxes(0, 1)
-    s = np.concatenate([segments, pts_ins_box]).reshape((2, -1, 3))
-    sums = segments_intersect_triangles(s, t)
-    ray_tracing_mask = sums % 2 != 0
-    mask[mask] = ray_tracing_mask
-    return mask
-
-
-#####################
-# facets computations
-#####################
-
-
-def facet_field(
+def magnet_facet_field(
     field: str,
     observers: np.ndarray,
     magnetization: np.ndarray,
     facets: np.ndarray,
 ) -> np.ndarray:
     """Magnetic field generated by homogeneous triangular surface charges.
+
+    Can be used to compute the field of a homogeneously magnetized body
+    with triangular surface mesh. Each facet must be defined so that the
+    normal vector points outwards.
 
     Parameters
     ----------
@@ -211,7 +95,7 @@ def facet_field(
     B = np.zeros((num_targets, 3))
 
     n = norm_vector(facets)
-    sigma = mydot(n, magnetization)
+    sigma = np.einsum("ij, ij->i", n, magnetization)
     R = []
     r = []
     for i in range(3):
@@ -225,7 +109,7 @@ def facet_field(
     QQ = np.zeros(num_targets)
     RR = np.zeros(num_targets)
     for i in range(3):
-        ii = next_i(i)
+        ii = (i+1)%3
         L = facets[:, ii] - facets[:, i]
         b = 2.0 * (R[i][:, 0] * L[:, 0] + R[i][:, 1] * L[:, 1] + R[i][:, 2] * L[:, 2])
         l = np.linalg.norm(L, axis=-1)
@@ -254,6 +138,115 @@ def facet_field(
 
     H = B * 10 / 4 / np.pi  # mT -> kA/m
     return H
+
+
+################
+# mask functions
+################
+
+
+# def mask_inside_enclosing_box(points, vertices, tol=1e-15):
+#     """Return a mask for `points` which truth value tells if inside the
+#     bounding box"""
+#     xmin, ymin, zmin = np.min(vertices, axis=0)
+#     xmax, ymax, zmax = np.max(vertices, axis=0)
+#     x, y, z = points.T
+#     # within cuboid dimension with positive tolerance
+#     mx = (x - xmax < tol) & (xmin - x < tol)
+#     my = (y - ymax < tol) & (ymin - y < tol)
+#     mz = (z - zmax < tol) & (zmin - z < tol)
+#     return mx & my & mz
+
+
+# def mask_inside_facets_convexhull(points, vertices):
+#     """Return a mask for `points` which truth value tells if inside the
+#     convexhulls built from provided `vertices`."""
+#     # check first if points are in enclosing box, to save costly convexhull computation
+#     inside_enclosing_box = mask_inside_enclosing_box(points, vertices)
+#     hull = ConvexHull(vertices, incremental=True)
+#     simplices_init = hull.simplices
+#     for ind, pt in enumerate(points):
+#         if inside_enclosing_box[ind]:
+#             hull.add_points([pt])
+#             if hull.simplices.shape != simplices_init.shape or not np.all(
+#                 hull.simplices == simplices_init
+#             ):
+#                 inside_enclosing_box[ind] = False
+#                 hull.close()
+#                 hull = ConvexHull(vertices, incremental=True)
+#     hull.close()
+#     return inside_enclosing_box
+
+
+# def signed_volume(a, b, c, d):
+#     """Computes the signed volume of a series of tetrahedrons defined by the vertices in
+#     a, b c and d. The ouput is an SxT array which gives the signed volume of the tetrahedron
+#     defined by the line segment 's' and two vertices of the triangle 't'."""
+
+#     return np.sum((a - d) * np.cross(b - d, c - d), axis=2)
+
+
+# def segments_intersect_triangles(s, t):
+#     """For each line segment in 's', this function computes how many times it intersects
+#     any of the triangles given in 't'."""
+#     # compute the normals to each triangle
+#     normals = np.cross(t[2] - t[0], t[2] - t[1])
+#     normals /= np.linalg.norm(normals, axis=1)[:, np.newaxis]
+
+#     # get sign of each segment endpoint, if the sign changes then we know this segment crosses the
+#     # plane which contains a triangle. If the value is zero the endpoint of the segment lies on the
+#     # plane.
+#     # s[i][:, np.newaxis] - t[j] -> S x T x 3 array
+#     sign1 = np.sign(np.sum(normals * (s[0][:, np.newaxis] - t[2]), axis=2))  # S x T
+#     sign2 = np.sign(np.sum(normals * (s[1][:, np.newaxis] - t[2]), axis=2))  # S x T
+
+#     # determine segments which cross the plane of a triangle.
+#     #  -> 1 if the sign of the end points of s is
+#     # different AND one of end points of s is not a vertex of t
+#     cross = (sign1 != sign2) * (sign1 != 0) * (sign2 != 0)  # S x T
+
+#     # get signed volumes
+#     v1 = np.sign(
+#         signed_volume(t[0], t[1], s[0][:, np.newaxis], s[1][:, np.newaxis])
+#     )  # S x T
+#     v2 = np.sign(
+#         signed_volume(t[1], t[2], s[0][:, np.newaxis], s[1][:, np.newaxis])
+#     )  # S x T
+#     v3 = np.sign(
+#         signed_volume(t[2], t[0], s[0][:, np.newaxis], s[1][:, np.newaxis])
+#     )  # S x T
+
+#     same_volume = np.logical_and(
+#         (v1 == v2), (v2 == v3)
+#     )  # 1 if s and t have same sign in v1, v2 and v3
+
+#     return np.sum(cross * same_volume, axis=1)
+
+
+# def mask_inside_trimesh(points, facets):
+#     """Return a boolean mask corresponding to the truth values of which points are inside
+#     the triangular mesh defined by the provided facets, using the ray tracing method"""
+#     # compute only points outside enclosing box more efficiently
+#     vertices = np.unique(facets.reshape((-1, 3)), axis=0)
+#     mask = mask_inside_enclosing_box(points, vertices)
+
+#     # choose a start point that is fore sure outside the mesh
+#     pts_ins_box = points[mask]
+#     start_point = np.min(vertices, axis=0) - np.array([1.001, 0.992, 0.993])
+#     segments = np.tile(start_point, (pts_ins_box.shape[0], 1))
+#     t = facets.swapaxes(0, 1)
+#     s = np.concatenate([segments, pts_ins_box]).reshape((2, -1, 3))
+#     sums = segments_intersect_triangles(s, t)
+#     ray_tracing_mask = sums % 2 != 0
+#     mask[mask] = ray_tracing_mask
+#     return mask
+
+
+#####################
+# facets computations
+#####################
+
+
 
 
 # def magnet_facets_field(
