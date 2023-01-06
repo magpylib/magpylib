494bf297fff28f9b3999611027129b7e392ff5b8
OrtnerMichael <magpylib@gmail.com>
2023-01-06 16:08:06 +0100

    minitestfix

--- tests/test__missing_optional_modules.py
+++ tests/test__missing_optional_modules.py
@@ -1,15 +1,13 @@
+import unittest
 import sys
-from unittest import mock
-
 import pytest
-
 import magpylib as magpy
 
 
 def test_show_with_missing_pyvista():
     """Should raise if pyvista is not installed"""
     src = magpy.magnet.Cuboid((0, 0, 1000), (1, 1, 1))
-    with mock.patch.dict(sys.modules, {"pyvista": None}):
+    with unittest.mock.patch.dict(sys.modules, {"pyvista": None}):
         # with pytest.raises(ModuleNotFoundError):
         src.show(return_fig=True, backend="pyvista")
 
@@ -17,6 +15,6 @@ def test_show_with_missing_pyvista():
 def test_dataframe_output_missing_pandas():
     """test if pandas is installed when using dataframe output in `getBH`"""
     src = magpy.magnet.Cuboid((0, 0, 1000), (1, 1, 1))
-    with mock.patch.dict(sys.modules, {"pandas": None}):
+    with unittest.mock.patch.dict(sys.modules, {"pandas": None}):
         with pytest.raises(ModuleNotFoundError):
             src.getB((0, 0, 0), output="dataframe")
