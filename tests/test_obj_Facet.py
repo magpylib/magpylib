86ea888f6debc2506d34d78e23fbf9cdfe0bd727
Alexandre Boisselet <alexabois@gmail.com>
2023-01-30 09:21:18 +0100

    add TriangularMesh body tests

--- tests/test_obj_Facet.py
+++ tests/test_obj_Facet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import magpylib as magpy
 from magpylib._src.exceptions import MagpylibMissingInput
@@ -84,3 +85,87 @@ def test_barycenter():
     face = magpy.misc.Triangle(mag, vert)
     bary = np.array([0, 0, 0])
     np.testing.assert_allclose(face.barycenter, bary)
+
+
+def test_triangular_mesh_body():
+    """Compare meshed cube to magpylib cube"""
+    dimension = (1, 1, 1)
+    magnetization = (100, 200, 300)
+    mesh3d = magpy.graphics.model3d.make_Cuboid()
+    points = np.array([v for k, v in mesh3d["kwargs"].items() if k in "xyz"]).T
+    triangles = np.array([v for k, v in mesh3d["kwargs"].items() if k in "ijk"]).T
+
+    triangles[0] = triangles[0][[0, 2, 1]]  # flip one triangle in wrong orientation
+
+    cube = magpy.magnet.Cuboid(magnetization=magnetization, dimension=dimension)
+    cube.rotate_from_angax(19, (1, 2, 3))
+    cube.move((1, 2, 3))
+
+    cube_facet_reorient_true = magpy.magnet.TriangularMesh(
+        position=cube.position,
+        orientation=cube.orientation,
+        magnetization=magnetization,
+        facets=points[triangles],
+        reorient_facets=True,
+    )
+    cube_facet_reorient_false = magpy.magnet.TriangularMesh(
+        position=cube.position,
+        orientation=cube.orientation,
+        magnetization=magnetization,
+        facets=points[triangles],
+        reorient_facets=False,
+    )
+
+    points = np.linspace((-2, 0, 0), (2, 0, 0), 50)
+    B1 = cube.getB(points)
+    B2 = cube_facet_reorient_true.getB(points)
+    B3 = cube_facet_reorient_false.getB(points)
+
+    np.testing.assert_allclose(B1, B2)
+
+    with pytest.raises(AssertionError):
+        np.testing.assert_allclose(B1, B3)
+
+
+def test_self_intersecting_triangular_mesh():
+    """raises Error if self intersecting"""
+    self_intersecting_mesh3d = {
+        "x": [-1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 0.0],
+        "y": [-1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 0.0],
+        "z": [-1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -2.0],
+        "i": [7, 0, 0, 0, 2, 6, 4, 0, 3, 7, 4, 5, 6, 7],
+        "j": [0, 7, 1, 2, 1, 2, 5, 5, 2, 2, 5, 6, 7, 4],
+        "k": [3, 4, 2, 3, 5, 5, 0, 1, 7, 6, 8, 8, 8, 8],
+    }
+    points = np.array([v for k, v in self_intersecting_mesh3d.items() if k in "xyz"]).T
+    triangles = np.array(
+        [v for k, v in self_intersecting_mesh3d.items() if k in "ijk"]
+    ).T
+    with pytest.raises(ValueError):
+        magpy.magnet.TriangularMesh.from_points(
+            magnetization=(0, 0, 1000),
+            points=points,
+            triangles=triangles,
+            validate_mesh=True,
+        )
+
+
+def test_open_mesh():
+    """raises Error if mesh is open"""
+    open_mesh = {
+        "i": [7, 0, 0, 0, 4, 4, 2, 6, 4, 0, 3],
+        "j": [0, 7, 1, 2, 6, 7, 1, 2, 5, 5, 2],
+        "k": [3, 4, 2, 3, 5, 6, 5, 5, 0, 1, 7],
+        "x": [-1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0],
+        "y": [-1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0],
+        "z": [-1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0],
+    }
+    points = np.array([v for k, v in open_mesh.items() if k in "xyz"]).T
+    triangles = np.array([v for k, v in open_mesh.items() if k in "ijk"]).T
+    with pytest.raises(ValueError):
+        magpy.magnet.TriangularMesh.from_points(
+            magnetization=(0, 0, 1000),
+            points=points,
+            triangles=triangles,
+            validate_mesh=True,
+        )
