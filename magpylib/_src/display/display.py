6b3447bd40172af0bfd362aa6286756c0653127c
Alexandre Boisselet <alexabois@gmail.com>
2023-02-16 19:52:16 +0100

    Add default values

--- magpylib/_src/display/display.py
+++ magpylib/_src/display/display.py
@@ -35,14 +35,15 @@ ctx = DisplayContext()
 
 
 RCO_NAMES = ("row", "col", "output", "sumup", "pixel_agg")
+DEFAULT_PLACEHOLDER = "<default>"
 
 
 def _show(
     *objects,
-    zoom=0,
-    animation=False,
-    markers=None,
     backend=None,
+    animation=False,
+    zoom=0,
+    markers=None,
     **kwargs,
 ):
     """Display objects and paths graphically.
@@ -101,7 +102,20 @@ def _show(
     )
 
 
-def show(*objects, **kwargs):
+def show(
+    *objects,
+    # pylint: disable=unused-argument
+    backend=DEFAULT_PLACEHOLDER,
+    animation=DEFAULT_PLACEHOLDER,
+    zoom=DEFAULT_PLACEHOLDER,
+    markers=DEFAULT_PLACEHOLDER,
+    row=DEFAULT_PLACEHOLDER,
+    col=DEFAULT_PLACEHOLDER,
+    output=DEFAULT_PLACEHOLDER,
+    sumup=DEFAULT_PLACEHOLDER,
+    pixel_agg=DEFAULT_PLACEHOLDER,
+    **kwargs,
+):
     """Display objects and paths graphically.
 
     Global graphic styles can be set with kwargs as style dictionary or using
@@ -209,7 +223,7 @@ def show(*objects, **kwargs):
     >>> fig.show() # doctest: +SKIP
     >>> # graphic output
     """
-
+    kwargs.update({k: v for k, v in locals().items() if v != DEFAULT_PLACEHOLDER})
     if ctx.isrunning:
         rco = {k: v for k, v in kwargs.items() if k in RCO_NAMES}
         ctx.kwargs.update({k: v for k, v in kwargs.items() if k not in RCO_NAMES})
@@ -221,12 +235,26 @@ def show(*objects, **kwargs):
 
 
 @contextmanager
-def show_context(*objects, **kwargs):
+def show_context(
+    *objects,
+    # pylint: disable=unused-argument
+    backend=DEFAULT_PLACEHOLDER,
+    animation=DEFAULT_PLACEHOLDER,
+    zoom=DEFAULT_PLACEHOLDER,
+    markers=DEFAULT_PLACEHOLDER,
+    row=DEFAULT_PLACEHOLDER,
+    col=DEFAULT_PLACEHOLDER,
+    output=DEFAULT_PLACEHOLDER,
+    sumup=DEFAULT_PLACEHOLDER,
+    pixel_agg=DEFAULT_PLACEHOLDER,
+    **kwargs,
+):
     """Context manager to temporarily set display settings in the `with` statement context.
 
     You need to invoke as ``show_context(pattern1=value1, pattern2=value2)``.
     """
     # pylint: disable=protected-access
+    kwargs.update({k: v for k, v in locals().items() if v != DEFAULT_PLACEHOLDER})
     try:
         ctx.isrunning = True
         rco = {k: v for k, v in kwargs.items() if k in RCO_NAMES}
