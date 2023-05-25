8ab6aebaa87f2fe6e52f083a5c9b264006421594
mortner <magpylib@gmail.com>
2023-05-25 12:49:24 +0200

    pylint

--- magpylib/_src/input_checks.py
+++ magpylib/_src/input_checks.py
@@ -562,7 +562,6 @@ def check_dimensions(sources):
     from magpylib._src.obj_classes.class_current_Line import Line
     from magpylib._src.obj_classes.class_current_Loop import Loop
     from magpylib._src.obj_classes.class_magnet_Tetrahedron import Tetrahedron
-    from magpylib._src.obj_classes.class_magnet_TriangularMesh import TriangularMesh
 
     for src in sources:
         if isinstance(src, (Cuboid, Cylinder, CylinderSegment)):
