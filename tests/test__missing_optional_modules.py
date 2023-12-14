f9a345c2f06e2cb704960601e417240a9baf3cbb
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2023-12-14 18:09:17 +0100

    udpate some tests

--- tests/test__missing_optional_modules.py
+++ tests/test__missing_optional_modules.py
@@ -1,4 +1,3 @@
-import sys
 from unittest.mock import patch
 
 import pytest
@@ -9,22 +8,22 @@ import magpylib as magpy
 def test_show_with_missing_pyvista():
     """Should raise if pyvista is not installed"""
     src = magpy.magnet.Cuboid((0, 0, 1000), (1, 1, 1))
-    with patch.dict(sys.modules, {"pyvista": None}):
-        # with pytest.raises(ModuleNotFoundError):
-        src.show(return_fig=True, backend="pyvista")
+    with patch.dict("sys.modules", {"pyvista": None}):
+        with pytest.raises(ModuleNotFoundError):
+            src.show(return_fig=True, backend="pyvista")
 
 
 def test_show_with_missing_mayavi():
     """Should raise if mayavi is not installed"""
     src = magpy.magnet.Cuboid((0, 0, 1000), (1, 1, 1))
-    with mock.patch.dict(sys.modules, {"mayavi": None}):
-        # with pytest.raises(ModuleNotFoundError):
-        src.show(return_fig=True, backend="mayavi")
+    with patch.dict("sys.modules", {"mayavi": None}):
+        with pytest.raises(ModuleNotFoundError):
+            src.show(return_fig=True, backend="mayavi")
 
 
 def test_dataframe_output_missing_pandas():
     """test if pandas is installed when using dataframe output in `getBH`"""
     src = magpy.magnet.Cuboid((0, 0, 1000), (1, 1, 1))
-    with patch.dict(sys.modules, {"pandas": None}):
+    with patch.dict("sys.modules", {"pandas": None}):
         with pytest.raises(ModuleNotFoundError):
             src.getB((0, 0, 0), output="dataframe")
