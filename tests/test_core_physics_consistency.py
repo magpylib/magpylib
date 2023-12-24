0e040204f8ba5b264ff0f909784f95b57be35ad5
mortner <magpylib@gmail.com>
2023-12-24 07:52:56 +0100

    core physics tests

---
+++ tests/test_core_physics_consistency.py
@@ -0,0 +1,440 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+
+import magpylib as magpy
+from magpylib._src.exceptions import MagpylibDeprecationWarning
+from magpylib._src.exceptions import MagpylibMissingInput
+from magpylib._src.fields.field_BH_polyline import current_vertices_field
+from magpylib._src.fields.field_BH_triangularmesh import magnet_trimesh_field
+from magpylib._src.utility import MU0
+from magpylib.core import current_circle_field
+from magpylib.core import current_line_field
+from magpylib.core import current_loop_field
+from magpylib.core import current_polyline_field
+from magpylib.core import dipole_field
+from magpylib.core import magnet_cuboid_field
+from magpylib.core import magnet_cylinder_field
+from magpylib.core import magnet_cylinder_segment_field
+from magpylib.core import magnet_sphere_field
+from magpylib.core import magnet_tetrahedron_field
+from magpylib.core import triangle_field
+
+# Magnetic moment of a current loop with current I and surface A:
+#   mom = I * A
+# Magnetic moment of a homogeneous magnet with magnetization mag and volume vol
+#   mom = vol * mag
+# Current density j on magnet surface in the replacement picture
+#   j = mag = J/MU0
+
+# ----------> Circle          # webpage numbers
+# Circle   <> Dipole          # mom = I*A (far field approx)
+# Polyline <> Dipole          # mom = I*A (far field approx)
+# Dipole   <> Sphere          # mom = vol*mag (similar outside of sphere)
+# Dipole   <> all Magnets     # mom = vol*mag (far field approx)
+# Circle   <> Cylinder        # j = I*N/L == J/MU0 current replacement picture
+# Polyline <> Cuboid          # j = I*N/L == J/MU0 current replacement picture
+# Circle   <> Polyline        # geometric approx
+# Cylinder <> CylinderSegment # geometric approx
+
+
+# Circle<>Dipole
+def test_core_phys_moment_of_current_circle():
+    """
+    test dipole vs circular current loop
+    moment = current * surface
+    far field test
+    """
+    obs = np.array([(10, 20, 30), (-10, -20, 30)])
+    dia = np.array([2, 2])
+    curr = np.array([1e3, 1e3])
+    mom = ((dia / 2) ** 2 * np.pi * curr * np.array([(0, 0, 1)] * 2).T).T
+
+    B1 = magpy.core.current_circle_field(
+        field="B",
+        observers=obs,
+        diameter=dia,
+        current=curr,
+    )
+    B2 = magpy.core.dipole_field(
+        field="B",
+        observers=obs,
+        moment=mom,
+    )
+    np.testing.assert_allclose(B1, B2, rtol=1e-02)
+
+    H1 = magpy.core.current_circle_field(
+        field="H",
+        observers=obs,
+        diameter=dia,
+        current=curr,
+    )
+    H2 = magpy.core.dipole_field(
+        field="H",
+        observers=obs,
+        moment=mom,
+    )
+    np.testing.assert_allclose(H1, H2, rtol=1e-02)
+
+
+# Polyline <> Dipole
+def test_core_phys_moment_of_current_square():
+    """
+    test dipole VS square current loop
+    moment = I x A, far field test
+    """
+    obs1 = np.array([(10, 20, 30)])
+    obs4 = np.array([(10, 20, 30)] * 4)
+    vert = np.array([(1, 1, 0), (1, -1, 0), (-1, -1, 0), (-1, 1, 0), (1, 1, 0)])
+    curr1 = 1e3
+    curr4 = np.array([curr1] * 4)
+    mom = (4 * curr1 * np.array([(0, 0, 1)]).T).T
+
+    B1 = magpy.core.current_polyline_field(
+        field="B",
+        observers=obs4,
+        segment_start=vert[:-1],
+        segment_end=vert[1:],
+        current=curr4,
+    )
+    B1 = np.sum(B1, axis=0)
+    B2 = magpy.core.dipole_field(
+        field="B",
+        observers=obs1,
+        moment=mom,
+    )[0]
+    np.testing.assert_allclose(B1, -B2, rtol=1e-03)
+
+    H1 = magpy.core.current_polyline_field(
+        field="H",
+        observers=obs4,
+        segment_start=vert[:-1],
+        segment_end=vert[1:],
+        current=curr4,
+    )
+    H1 = np.sum(H1, axis=0)
+    H2 = magpy.core.dipole_field(
+        field="H",
+        observers=obs1,
+        moment=mom,
+    )[0]
+    np.testing.assert_allclose(H1, -H2, rtol=1e-03)
+
+
+# Circle <> Polyline
+def test_core_phys_circle_polyline():
+    """approximate circle with polyline"""
+    ts = np.linspace(0, 2 * np.pi, 300)
+    vert = np.array([(np.sin(t), np.cos(t), 0) for t in ts])
+    curr = np.array([1])
+    curr99 = np.array([1] * 299)
+    obs = np.array([(1, 2, 3)])
+    obs99 = np.array([(1, 2, 3)] * 299)
+    dia = np.array([2])
+
+    H1 = magpy.core.current_circle_field(
+        field="H",
+        observers=obs,
+        diameter=dia,
+        current=curr,
+    )[0]
+    H2 = magpy.core.current_polyline_field(
+        field="H",
+        observers=obs99,
+        segment_start=vert[:-1],
+        segment_end=vert[1:],
+        current=curr99,
+    )
+    H2 = np.sum(H2, axis=0)
+    np.testing.assert_allclose(H1, -H2, rtol=1e-4)
+
+    B1 = magpy.core.current_circle_field(
+        field="B",
+        observers=obs,
+        diameter=dia,
+        current=curr,
+    )[0]
+    B2 = magpy.core.current_polyline_field(
+        field="B",
+        observers=obs99,
+        segment_start=vert[:-1],
+        segment_end=vert[1:],
+        current=curr99,
+    )
+    B2 = np.sum(B2, axis=0)
+    np.testing.assert_allclose(B1, -B2, rtol=1e-4)
+
+
+# Dipole <> Sphere
+def test_core_physics_dipole_sphere():
+    """
+    dipole and sphere field must be similar outside of sphere
+    moment = magnetization * volume
+    near field tests
+    """
+    obs = np.array([(1, 2, 3), (-2, -2, -2), (3, 5, -4), (5, 4, 0.1)])
+    dia = np.array([2, 3, 0.1, 3.3])
+    pol = np.array([(1, 2, 3), (0, 0, 1), (-1, -2, 0), (1, -1, 0.1)])
+    mom = np.array([4 * (d / 2) ** 3 * np.pi / 3 * p / MU0 for d, p in zip(dia, pol)])
+
+    B1 = magpy.core.magnet_sphere_field(
+        field="B",
+        observers=obs,
+        diameter=dia,
+        polarization=pol,
+    )
+    B2 = magpy.core.dipole_field(
+        field="B",
+        observers=obs,
+        moment=mom,
+    )
+    np.testing.assert_allclose(B1, B2, rtol=0, atol=1e-16)
+
+    H1 = magpy.core.magnet_sphere_field(
+        field="H",
+        observers=obs,
+        diameter=dia,
+        polarization=pol,
+    )
+    H2 = magpy.core.dipole_field(
+        field="H",
+        observers=obs,
+        moment=mom,
+    )
+    np.testing.assert_allclose(H1, H2, rtol=0, atol=1e-11)
+
+
+# -> Circle, Cylinder
+def test_core_physics_long_solenoid():
+    """
+    test if field from solenoid converges to long-solenoid field
+    Hz = I*N/L "in the center"
+    """
+    I = 1
+    N = 10000
+    R = 0.8
+    L = 100
+    Hz_long = N * I / L
+
+    # test with solenoid constructed from circle fields
+    H = magpy.core.current_circle_field(
+        field="H",
+        observers=np.linspace((0, 0, -L / 2), (0, 0, L / 2), N),
+        diameter=np.array([2 * R] * N),
+        current=np.array([I] * N),
+    )
+    Hz_sol = np.sum(H, axis=0)[2]
+    np.testing.assert_allclose(Hz_sol, Hz_long, rtol=1e-3)
+
+    # test with cylinder field (using current replacement)
+    Jz = MU0 * I * N / L  # polarization <> current density
+    Hz_cyl = magpy.core.magnet_cylinder_field(
+        field="H",
+        observers=np.array([(0, 0, 0)]),
+        dimension=np.array([(2 * R, L)]),
+        polarization=np.array([(0, 0, Jz)]),
+    )[0, 2]
+    np.testing.assert_allclose(Hz_cyl, Hz_long, rtol=1e-3)
+
+
+# Circle<>Cylinder
+def test_core_physics_current_replacement():
+    """
+    test if the Cylinder field is given by a replacement current sheet
+    that carries a current density of j=magnetization
+    It follows:
+        j = I*N/L == J/MU0
+          -> I = J/MU0/N*L
+    near-field test
+    """
+    L = 0.5
+    R = 0.987
+    obs = np.array([(1.5, -2, -1.123)])
+
+    Jz = 1
+    Hz_cyl = magpy.core.magnet_cylinder_field(
+        field="H",
+        observers=obs,
+        dimension=np.array([(2 * R, L)]),
+        polarization=np.array([(0, 0, Jz)]),
+    )[0, 2]
+
+    N = 1000  # current discretization
+    I = Jz / MU0 / N * L
+    H = magpy.core.current_circle_field(
+        field="H",
+        observers=np.linspace((0, 0, -L / 2), (0, 0, L / 2), N) + obs,
+        diameter=np.array([2 * R] * N),
+        current=np.array([I] * N),
+    )
+    Hz_curr = np.sum(H, axis=0)[2]
+
+    np.testing.assert_allclose(Hz_curr, Hz_cyl, rtol=1e-4)
+
+
+# Cylinder<>CylinderSegment
+def test_core_physics_geometry_cylinder_from_segments():
+    """test if multiple Cylinder segments create the same field as fully cylinder"""
+    r = 1.23
+    h = 3
+    obs = np.array([(1, 2, 3), (0.23, 0.132, 0.123)])
+    pol = np.array([(2, 0.123, 3), (-0.23, -1, 0.434)])
+
+    B_cyl = magpy.core.magnet_cylinder_field(
+        field="B",
+        observers=obs,
+        dimension=np.array([(2 * r, h)] * 2),
+        polarization=pol,
+    )
+    sections = np.array([-12, 65, 123, 180, 245, 348])
+
+    Bseg = np.zeros((2, 3))
+    for phi1, phi2 in zip(sections[:-1], sections[1:]):
+        B_part = magpy.core.magnet_cylinder_segment_field(
+            field="B",
+            observers=obs,
+            dimension=np.array([(0, r, h, phi1, phi2)] * 2),
+            polarization=pol,
+        )
+        Bseg[0] += B_part[0]
+        Bseg[1] += B_part[1]
+    np.testing.assert_allclose(B_cyl, Bseg)
+
+
+# Dipole<>Cuboid, Cylinder, CylinderSegment, Tetrahedron
+def test_core_physics_dipole_approximation_magnet_far_field():
+    """test if all magnets satisfy the dipole approximation"""
+    obs = np.array([(100, 200, 300), (-200, -200, -200)])
+
+    mom = np.array([(1e6, 2e6, 3e6)] * 2)
+    Bdip = magpy.core.dipole_field(
+        field="H",
+        observers=obs,
+        moment=mom,
+    )
+
+    dim = np.array([(2, 2, 2)] * 2)
+    vol = 8
+    pol = mom / vol * MU0
+    Bcub = magpy.core.magnet_cuboid_field(
+        field="H",
+        observers=obs,
+        dimension=dim,
+        polarization=pol,
+    )
+    np.testing.assert_allclose(Bdip, Bcub)
+
+    dim = np.array([(0.5, 0.5)] * 2)
+    vol = 0.25**2 * np.pi * 0.5
+    pol = mom / vol * MU0
+    Bcyl = magpy.core.magnet_cylinder_field(
+        field="H",
+        observers=obs,
+        dimension=dim,
+        polarization=pol,
+    )
+    np.testing.assert_allclose(Bdip, Bcyl)
+
+    vert = np.array([[(0, 0, 0), (0, 0, 0.1), (0.1, 0, 0), (0, 0.1, 0)]] * 2)
+    vol = 1 / 6 * 1e-3
+    pol = mom / vol * MU0
+    Btetra = magpy.core.magnet_tetrahedron_field(
+        field="H",
+        observers=obs,
+        vertices=vert,
+        polarization=pol,
+    )
+    np.testing.assert_allclose(Bdip, Btetra, rtol=1e-3)
+
+    dim = np.array([(0.1, 0.2, 0.1, -25, 25)] * 2)
+    vol = 3 * np.pi * (50 / 360) * 1e-3
+    pol = mom / vol * MU0
+    Bcys = magpy.core.magnet_cylinder_segment_field(
+        field="H",
+        observers=obs + np.array((0.15, 0, 0)),
+        dimension=dim,
+        polarization=pol,
+    )
+    np.testing.assert_allclose(Bdip, Bcys, rtol=1e-4)
+
+
+# --> Circle
+def test_core_physics_circle_VS_webpage_numbers():
+    """
+    Compare Circle on-axis field vs e-magnetica & hyperphysics
+    """
+    dia = np.array([2] * 4)
+    curr = np.array([1e3] * 4)  # A
+    zs = [0, 1, 2, 3]
+    obs = np.array([(0, 0, z) for z in zs])
+
+    # values from e-magnetica
+    Hz = [500, 176.8, 44.72, 15.81]
+    Htest = [(0, 0, hz) for hz in Hz]
+
+    H = magpy.core.current_circle_field(
+        field="H",
+        observers=obs,
+        diameter=dia,
+        current=curr,
+    )
+    np.testing.assert_allclose(H, Htest, rtol=1e-3)
+
+    # values from hyperphysics
+    Bz = [
+        0.6283185307179586e-3,
+        2.2214414690791835e-4,
+        5.619851784832581e-5,
+        1.9869176531592205e-5,
+    ]
+    Btest = [(0, 0, bz) for bz in Bz]
+
+    B = magpy.core.current_circle_field(
+        field="B",
+        observers=obs,
+        diameter=dia,
+        current=curr,
+    )
+    np.testing.assert_allclose(B, Btest, rtol=1e-7)
+
+
+# Cuboid <> Polyline
+def test_core_physics_cube_current_replacement():
+    """compare cuboid field with current replacement"""
+    obs = np.array([(2, 2, 3.13), (-2.123, -4, 2)])
+    h = 1
+    Jz = 1.23
+    dim = np.array([(2, 2, h)] * 2)
+    pol = np.array([(0, 0, Jz)] * 2)
+    Hcub = magpy.core.magnet_cuboid_field(
+        field="H",
+        observers=obs,
+        dimension=dim,
+        polarization=pol,
+    )
+
+    # construct from polylines
+    n = 1000
+    vert = np.array([(1, 1, 0), (1, -1, 0), (-1, -1, 0), (-1, 1, 0), (1, 1, 0)])
+    curr = h / n * Jz / MU0
+    hs = np.linspace(-h / 2, h / 2, n)
+    hpos = np.array([(0, 0, h) for h in hs])
+
+    obs1 = np.array([obs[0] + hp for hp in hpos] * 4)
+    obs2 = np.array([obs[1] + hp for hp in hpos] * 4)
+
+    start = np.repeat(vert[:-1], n, axis=0)
+    end = np.repeat(vert[1:], n, axis=0)
+
+    Hcurr = np.zeros((2, 3))
+    for i, obss in enumerate([obs1, obs2]):
+        h = magpy.core.current_polyline_field(
+            field="H",
+            observers=obss,
+            segment_start=start,
+            segment_end=end,
+            current=np.array([curr] * 4 * n),
+        )
+        Hcurr[i] = np.sum(h, axis=0)
+
+    np.testing.assert_allclose(Hcub, -Hcurr, rtol=1e-4)
