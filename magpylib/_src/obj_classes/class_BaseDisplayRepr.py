df8dafada7c39c7ff7f150997603b36cb947f1af
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2024-01-08 17:36:04 +0100

    fix disp repr

--- magpylib/_src/obj_classes/class_BaseDisplayRepr.py
+++ magpylib/_src/obj_classes/class_BaseDisplayRepr.py
@@ -46,41 +46,41 @@ class BaseDisplayRepr:
             exclude = ()
         params = list(self._property_names_generator())
         lines = [f"{self!r}"]
-        for k in list(dict.fromkeys(list(UNITS) + list(params))):
-            if not k.startswith("_") and k in params and k not in exclude:
-                val, val_str = getattr(self, k), None
+        for key in list(dict.fromkeys(list(UNITS) + list(params))):
+            if not key.startswith("_") and key in params and key not in exclude:
+                val, val_str = getattr(self, key), None
+                unit = UNITS.get(key, None)
+                if ureg is not None and isinstance(val, ureg.Quantity):
+                    # let pint take care of displaying units if available
+                    unit = None
                 if val is None:
                     val_str = None
-                elif k == "position":
+                elif key == "position":
                     v = getattr(self, "_position")
                     if v.shape[0] != 1:
                         lines.append(f"  • path length: {v.shape[0]}")
-                        k = f"{k} (last)"
+                        key = f"{key} (last)"
                     val_str = f"{v[-1]}"
-                elif k == "orientation":
+                elif key == "orientation":
                     v = getattr(self, "_orientation")
                     v = v.as_rotvec(degrees=True)
                     if len(v) != 1:
-                        k = f"{k} (last)"
+                        key = f"{key} (last)"
                     val_str = f"{v[-1]}"
-                elif k == "pixel":
+                elif key == "pixel":
                     px_shape = val.shape[:-1]
                     val_str = f"{int(np.prod(px_shape))}"
                     if val.ndim > 2:
                         val_str += f" ({'x'.join(str(p) for p in px_shape)})"
-                elif k == "status_disconnected_data":
+                elif key == "status_disconnected_data":
                     val_str = f"{len(val)} part{'s'[:len(val)^1]}"
                 elif isinstance(val, (list, tuple, np.ndarray)):
                     v = np.array(val)
                     if np.prod(v.shape) > 4:
                         val_str = f"shape{v.shape}"
-                unit = UNITS.get(k, None)
-                if ureg is not None and isinstance(val, ureg.Quantity):
-                    # let pint take care of displaying units if available
-                    unit = None
                 unit_str = f" {unit}" if unit else ""
                 val_str = val if val_str is None else val_str
-                lines.append(f"  • {k}: {val_str}{unit_str}")
+                lines.append(f"  • {key}: {val_str}{unit_str}")
         return lines
 
     def describe(self, *, exclude=("style", "field_func"), return_string=False):
