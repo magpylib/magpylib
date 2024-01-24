fix expected units in python syntax

magpylib/_src/display/traces_core.py
@@ -153,7 +153,7 @@ def make_Dipole(obj, autosize=None, **kwargs) -> Dict[str, Any]:
     """
     style = obj.style
     moment = np.array([0.0, 0.0, 0.0]) if obj.moment is None else obj.moment
-    moment = downcast(moment, "A·m²")
+    moment = downcast(moment, "A*m**2")
     moment_mag = np.linalg.norm(moment)
     size = style.size
     if autosize is not None and style.sizemode == "scaled":

magpylib/_src/obj_classes/class_misc_Dipole.py
@@ -6,6 +6,7 @@ from magpylib._src.fields.field_BH_dipole import dipole_field
 from magpylib._src.input_checks import check_format_input_vector
 from magpylib._src.obj_classes.class_BaseExcitations import BaseSource
 from magpylib._src.style import DipoleStyle
+from magpylib._src.units import downcast
 from magpylib._src.units import unit_prefix
 
 
@@ -117,13 +118,17 @@ class Dipole(BaseSource):
             sig_name=f"{self.__class__.__name__}.moment",
             sig_type="array_like (list, tuple, ndarray) with shape (3,)",
             allow_None=True,
-            unit="A·m²",
+            unit="A*m**2",  # python syntax necessary for astropy, unyt
         )
 
     @property
     def _default_style_description(self):
         """Default style description text"""
-        moment = np.array([0.0, 0.0, 0.0]) if self.moment is None else self.moment
+        moment = (
+            np.array([0.0, 0.0, 0.0])
+            if self.moment is None
+            else downcast(self.moment, "A*m**2")
+        )
         moment_mag = np.linalg.norm(moment)
         if moment_mag == 0:
             return "no moment"