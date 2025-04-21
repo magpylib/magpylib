4e06382aee93cb6b9828ef36cc77b346be4b0f4d
Alexandre Boisselet <alexabois@gmail.com>
2025-04-22 01:35:49 +0200

    fix tests

--- tests/test_Coumpound_setters.py
+++ tests/test_Coumpound_setters.py
@@ -8,6 +8,9 @@ import numpy as np
 import pytest
 from scipy.spatial.transform import Rotation as R  # noqa: F401
 
+from scipy.spatial.transform import Rotation as R # noqa: F401
+
+
 import magpylib as magpy
 from magpylib._src.display.traces_base import make_Prism
 
