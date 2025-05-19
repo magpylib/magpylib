4ced289eac5d03e8a3f23acc77af709ebdc2ad57
purepani <pani0028@umn.edu>
2025-05-19 14:00:16 -0500

    Adds array api suppport for elliptic functions

--- src/magpylib/_src/fields/special_cel.py
+++ src/magpylib/_src/fields/special_cel.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 
 import math as m
 
+import array_api_extra as xpx
 import numpy as np
+from array_api_compat import array_namespace
 
 
 def cel0(kc, p, c, s):
@@ -56,63 +58,74 @@ def celv(kc, p, c, s):
     """
     vectorized version of the cel integral above
     """
+    xp = array_namespace(kc, p, c, s)
 
     # if kc == 0:
     #    return NaN
+    # errtol = 0.000001
     errtol = 0.000001
-    n = len(kc)
+    n = kc.shape[0]
 
-    k = np.abs(kc)
-    em = np.ones(n, dtype=float)
+    k = xp.abs(kc)
+    em = xp.ones(n, dtype=xp.float64)
 
-    cc = c.copy()
-    pp = p.copy()
-    ss = s.copy()
+    # cc = xp.asarray(c, copy=True)
+    # pp = xp.asarray(p, copy=True)
+    # ss = xp.asarray(s, copy=True)
 
     # apply a mask for evaluation of respective cases
     mask = p <= 0
 
-    # if p>0:
-    pp[~mask] = np.sqrt(p[~mask])
-    ss[~mask] = s[~mask] / pp[~mask]
-
-    # else:
-    f = kc[mask] * kc[mask]
+    f = kc * kc
     q = 1.0 - f
-    g = 1.0 - pp[mask]
-    f = f - pp[mask]
-    q = q * (ss[mask] - c[mask] * pp[mask])
-    pp[mask] = np.sqrt(f / g)
-    cc[mask] = (c[mask] - ss[mask]) / g
-    ss[mask] = -q / (g * g * pp[mask]) + cc[mask] * pp[mask]
+    g = 1.0 - p
+    f = f - p
+    q = q * (s - c * p)
+    pp = xpx.apply_where(
+        mask,
+        (p, f, g),
+        lambda p, f, g: xp.sqrt(f / g),
+        lambda p, f, g: xp.sqrt(p),
+    )
+    cc = xpx.apply_where(
+        mask, (c, s, g), lambda c, s, g: (c - s) / g, lambda c, ss, g: c
+    )
+    ss = xpx.apply_where(
+        mask,
+        (s, pp, q, g, cc),
+        lambda s, pp, q, g, cc: -q / (g * g * pp) + cc * pp,
+        lambda s, pp, q, g, cc: s / pp,
+    )
 
-    f = cc.copy()
+    f = xp.asarray(cc, copy=True)
     cc = cc + ss / pp
     g = k / pp
     ss = 2 * (ss + f * g)
     pp = g + pp
-    g = em.copy()
+    g = xp.asarray(em, copy=True)
     em = k + em
-    kk = k.copy()
+    kk = xp.asarray(k, copy=True)
 
     # define a mask that adjusts with every evaluation step so that only
     # non-converged entries are further iterated.
-    mask = np.ones(n, dtype=bool)
-    while np.any(mask):
-        k[mask] = 2 * np.sqrt(kk[mask])
-        kk[mask] = k[mask] * em[mask]
-        f[mask] = cc[mask]
-        cc[mask] = cc[mask] + ss[mask] / pp[mask]
-        g[mask] = kk[mask] / pp[mask]
-        ss[mask] = 2 * (ss[mask] + f[mask] * g[mask])
-        pp[mask] = g[mask] + pp[mask]
-        g[mask] = em[mask]
-        em[mask] = k[mask] + em[mask]
+    mask = xp.ones(n, dtype=xp.bool)
+    while xp.any(mask):
+        k = 2 * xp.sqrt(kk)
+        kk = k * em
+        f = cc
+        cc = cc + ss / pp
+        g = kk / pp
+        ss = 2 * (ss + f * g)
+        pp = g + pp
+        g = em
+        em = k + em
 
         # redefine mask
-        mask = np.abs(g - k) > g * errtol
+        err = g - k
+        tol = g * errtol
+        mask = xp.abs(err) > tol
 
-    return (np.pi / 2) * (ss + cc * em) / (em * (em + pp))
+    return (xp.pi / 2) * (ss + cc * em) / (em * (em + pp))
 
 
 def cel(kcv: np.ndarray, pv: np.ndarray, cv: np.ndarray, sv: np.ndarray) -> np.ndarray:
@@ -128,12 +141,12 @@ def cel(kcv: np.ndarray, pv: np.ndarray, cv: np.ndarray, sv: np.ndarray) -> np.n
     def ellipticPi(x, y):
         return elliptic((1-y)**(1/2.), 1-x, 1, 1)
     """
-    n_input = len(kcv)
+    # n_input = len(kcv)
 
-    if n_input < 10:
-        return np.array(
-            [cel0(kc, p, c, s) for kc, p, c, s in zip(kcv, pv, cv, sv, strict=False)]
-        )
+    # if n_input < 10:
+    # return np.array(
+    # [cel0(kc, p, c, s) for kc, p, c, s in zip(kcv, pv, cv, sv, strict=False)]
+    # )
 
     return celv(kcv, pv, cv, sv)
 
@@ -146,11 +159,11 @@ def cel_iter(qc, p, g, cc, ss, em, kk):
     #   This cannot happen in core functions
 
     # case2: small input vector - loop is faster than vectorized computation
-    n_input = len(qc)
-    if n_input < 15:
-        result = np.zeros(n_input)
-        for i in range(n_input):
-            result[i] = cel_iter0(qc[i], p[i], g[i], cc[i], ss[i], em[i], kk[i])
+    # n_input = len(qc)
+    # if n_input < 15:
+    #    result = np.zeros(n_input)
+    #    for i in range(n_input):
+    #        result[i] = cel_iter0(qc[i], p[i], g[i], cc[i], ss[i], em[i], kk[i])
 
     # case3: vectorized evaluation
     return cel_iterv(qc, p, g, cc, ss, em, kk)
@@ -177,8 +190,9 @@ def cel_iterv(qc, p, g, cc, ss, em, kk):
     """
     Iterative part of Bulirsch cel algorithm
     """
-    while np.any(np.fabs(g - qc) >= qc * 1e-8):
-        qc = 2 * np.sqrt(kk)
+    xp = array_namespace(qc, p, g, cc, ss, em, kk)
+    while xp.any(xp.abs(g - qc) >= qc * 1e-8):
+        qc = 2 * xp.sqrt(kk)
         kk = qc * em
         f = cc
         cc = cc + ss / p
