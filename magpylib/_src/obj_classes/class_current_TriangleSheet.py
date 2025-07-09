818ee3c57c53305bd446c0892aac33c1eae48001
mortner <magpylib@gmail.com>
2025-07-09 06:56:05 +0200

    trisheet

---
+++ magpylib/_src/obj_classes/class_current_TriangleSheet.py
@@ -0,0 +1,113 @@
+# pylint: disable=too-many-positional-arguments
+
+"""TriangleSheet current class code"""
+
+from magpylib._src.display.traces_core import make_TriangleSheet
+from magpylib._src.fields.field_BH_current_sheet import BHJM_current_sheet
+from magpylib._src.input_checks import check_format_input_vertices
+from magpylib._src.utility import unit_prefix
+
+class TriangleStrip():
+    """Surface current density flowing along triangular sheets.
+
+    Can be used as `sources` input for magnetic field computation.
+
+    The vertex positions are defined in the local object coordinates (rotate with object).
+    When `position=(0,0,0)` and `orientation=None` global and local coordinates coincide.
+
+    SI units are used for all inputs and outputs.
+
+    Parameters
+    ----------
+    position: array_like, shape (3,) or (m,3), default=`(0,0,0)`
+        Object position(s) in the global coordinates in units of m. For m>1, the
+        `position` and `orientation` attributes together represent an object path.
+
+    orientation: scipy `Rotation` object with length 1 or m, default=`None`
+        Object orientation(s) in the global coordinates. `None` corresponds to
+        a unit-rotation. For m>1, the `position` and `orientation` attributes
+        together represent an object path.
+
+    vertices: array_like, shape (n,3), default=`None`
+        A set of points in units of m in the local object coordinates from which the
+        triangular faces are constructed by the additional `faces` input.
+
+    faces: array_like, shape (n,3), default=`None`
+        Indices of vertices. Each triplet represents one triangle of the mesh.
+
+    current_densities: array_like, shape (n,3), default=`None`
+        Electrical current in units of A. It is transformed into a homogeneous current
+        density which flows along the Triangles in direction: T1: V1->V3, T2: V2->V4, ...
+
+    parent: `Collection` object or `None`
+        The object is a child of it's parent collection.
+
+    style: dict
+        Object style inputs must be in dictionary form, e.g. `{'color':'red'}` or
+        using style underscore magic, e.g. `style_color='red'`.
+
+    Returns
+    -------
+    current source: `TriangleStrip` object
+
+    Examples
+    --------
+    `TriangleStrip` objects are magnetic field sources. In this example we compute the H-field in A/m
+    of a square current sheet (two triangles) with 1 A current at the observer position (1,1,1) cm:
+
+    >>> import numpy as np
+    >>> import magpylib as magpy
+    >>> src = magpy.current.TriangleStrip(
+    ...    current=1,
+    ...    vertices=((0,0,0), (0,1,0), (1,0,0), (1,1,0)),
+    ... )
+    >>> H = src.getH((.01,.01,.01))
+    >>> with np.printoptions(precision=3):
+    ...     print(H)
+    [ 3.965e-17 -2.905e-01 -3.747e-01]
+
+    """
+
+    # pylint: disable=dangerous-default-value
+    _field_func = staticmethod(BHJM_current_strip)
+    _field_func_kwargs_ndim = {
+        "current": 1,
+        "vertices": 3,
+    }
+    get_trace = make_TriangleStrip
+
+    def __init__(
+        self,
+        current=None,
+        vertices=None,
+        position=(0, 0, 0),
+        orientation=None,
+        style=None,
+        **kwargs,
+    ):
+        # instance attributes
+        self.vertices = vertices
+
+        # init inheritance
+        super().__init__(position, orientation, current, style, **kwargs)
+
+    # property getters and setters
+    @property
+    def vertices(self):
+        """
+        The current flows along the triangles ...
+        """
+        return self._vertices
+
+    @vertices.setter
+    def vertices(self, vert):
+        """Set TriangleStrip vertices, array_like, meter."""
+        self._vertices = check_format_input_vertices(vert)
+
+    @property
+    def _default_style_description(self):
+        """Default style description text"""
+        if self.vertices is None:
+            return "no vertices"
+        return f"{unit_prefix(self.current)}A" if self.current else "no current"
+
